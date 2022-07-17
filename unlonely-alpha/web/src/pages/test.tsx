d92f8812ee8a3126a68672d5078a0f1f248114f6
Brian Guan <bguan2020@ucla.edu>
2022-07-16 18:54:29 -0700

    new features

--- unlonely-alpha/web/src/pages/test.tsx
+++ unlonely-alpha/web/src/pages/test.tsx
@@ -1,7 +1,7 @@
 import { RoomProvider, useMyPresence, useOthers } from "@liveblocks/react";
 import { gql, useQuery } from "@apollo/client";
 import React, { useState, useCallback, useEffect } from "react";
-import { Text, Flex, Grid, GridItem } from "@chakra-ui/react";
+import { Text, Flex, Grid, GridItem, Switch } from "@chakra-ui/react";
 import { useAccount } from "wagmi";
 
 import Cursor from "../components/chat/Cursor";
@@ -39,6 +39,7 @@ function Example() {
   const [state, setState] = useState<CursorState>({ mode: CursorMode.Hidden });
   const [reactions, setReactions] = useState<Reaction[]>([]);
   const [sortVideoAs, setSortVideoAs] = useState<VideoAttribute>("score");
+  const [showCursor, setShowCursor] = useState<boolean>(true);
   const [username, setUsername] = useState<string | null>();
   const [{ data: accountData }] = useAccount();
 
@@ -69,6 +70,11 @@ function Example() {
     }
   };
 
+  const toggleHideCursor = () => {
+    if (showCursor) setShowCursor(false);
+    else setShowCursor(true);
+  };
+
   useEffect(() => {
     const fetchEns = async () => {
       if (accountData?.address) {
@@ -91,132 +97,245 @@ function Example() {
       >
         <GridItem rowSpan={1} colSpan={2}></GridItem>
         <GridItem rowSpan={3} colSpan={1} border="2px">
-          The Chat Room! Move your mouse here to chat!
-          <div
-            style={{
-              height: "100%",
-              width: "100%",
-              cursor:
-                state.mode === CursorMode.Chat
-                  ? "none"
-                  : "url(cursor.svg) 0 0, auto",
-            }}
-            onPointerMove={(event) => {
-              if (
-                cursor == null ||
-                state.mode !== CursorMode.ReactionSelector
-              ) {
-                updateMyPresence({
-                  cursor: {
-                    x: Math.round(event.clientX),
-                    y: Math.round(event.clientY),
-                  },
+          <Flex justifyContent="center" direction="column">
+            <Text fontWeight={"bold"} fontSize="20px">
+              The Chat Room!
+            </Text>
+            <Switch onChange={() => toggleHideCursor()}>
+              Hide Chat Cursor
+            </Switch>
+          </Flex>
+          {showCursor ? (
+            <div
+              style={{
+                height: "100%",
+                width: "100%",
+                cursor:
+                  state.mode === CursorMode.Chat
+                    ? "none"
+                    : "url(cursor.svg) 0 0, auto",
+              }}
+              onPointerMove={(event) => {
+                if (
+                  cursor == null ||
+                  state.mode !== CursorMode.ReactionSelector
+                ) {
+                  updateMyPresence({
+                    cursor: {
+                      x: Math.round(event.clientX),
+                      y: Math.round(event.clientY),
+                    },
+                  });
+                }
+              }}
+              onPointerLeave={() => {
+                setState({
+                  mode: CursorMode.Hidden,
                 });
-              }
-            }}
-            onPointerLeave={() => {
-              setState({
-                mode: CursorMode.Hidden,
-              });
-              updateMyPresence({
-                cursor: null,
-              });
-            }}
-            onClick={() => openComment()}
-          >
-            {reactions.map((reaction) => {
-              return (
-                <FlyingReaction
-                  key={reaction.timestamp.toString()}
-                  x={reaction.point.x}
-                  y={reaction.point.y}
-                  timestamp={reaction.timestamp}
-                  value={reaction.value}
-                />
-              );
-            })}
-            {cursor && (
-              <div
-                className="absolute top-0 left-0"
-                style={{
-                  transform: `translateX(${cursor.x}px) translateY(${cursor.y}px)`,
-                }}
-              >
-                {state.mode === CursorMode.Hidden && (
-                  <Flex maxW="120px">
-                    <Text noOfLines={4} fontSize="sm" color="black">
-                      click anywhere around the video to comment!
-                    </Text>
-                  </Flex>
-                )}
-                {state.mode === CursorMode.Chat && (
-                  <>
-                    <Flex direction="row">
-                      <img src="cursor.svg" />
-                      <Text>{username}</Text>
-                    </Flex>
-                    <div
-                      className="absolute top-5 left-2 px-4 py-2 bg-blue-500 text-white leading-relaxed text-sm"
-                      onKeyUp={(e) => e.stopPropagation()}
-                      style={{
-                        borderRadius: 20,
-                      }}
-                    >
-                      {state.previousMessage && (
-                        <div>{state.previousMessage}</div>
-                      )}
-                      <textarea
-                        className="bg-transparent border-none	outline-none text-white placeholder-blue-300 w-60"
-                        autoFocus={true}
-                        onChange={(e) => {
-                          updateMyPresence({ message: e.target.value });
-                          setState({
-                            mode: CursorMode.Chat,
-                            previousMessage: null,
-                            message: e.target.value,
-                          });
-                        }}
-                        placeholder={
-                          state.previousMessage ? "" : "Say something…"
-                        }
-                        value={state.message}
-                        maxLength={50}
-                      />
-                    </div>
-                  </>
-                )}
-                {state.mode === CursorMode.ReactionSelector && (
-                  <ReactionSelector
-                    setReaction={(reaction) => {
-                      setReaction(reaction);
-                    }}
+                updateMyPresence({
+                  cursor: null,
+                });
+              }}
+              onClick={() => openComment()}
+            >
+              {reactions.map((reaction) => {
+                return (
+                  <FlyingReaction
+                    key={reaction.timestamp.toString()}
+                    x={reaction.point.x}
+                    y={reaction.point.y}
+                    timestamp={reaction.timestamp}
+                    value={reaction.value}
                   />
-                )}
-                {state.mode === CursorMode.Reaction && (
-                  <div className="absolute top-3.5 left-1 pointer-events-none select-none">
-                    {state.reaction}
-                  </div>
-                )}
-              </div>
-            )}
-            {others.map(({ connectionId, presence }) => {
-              if (presence == null || !presence.cursor) {
-                return null;
-              }
+                );
+              })}
+              {cursor && (
+                <div
+                  className="absolute top-0 left-0"
+                  style={{
+                    transform: `translateX(${cursor.x}px) translateY(${cursor.y}px)`,
+                  }}
+                >
+                  {state.mode === CursorMode.Hidden && (
+                    <Flex maxW="120px">
+                      <Text noOfLines={4} fontSize="sm" color="black">
+                        click anywhere around the video to comment!
+                      </Text>
+                    </Flex>
+                  )}
+                  {state.mode === CursorMode.Chat && (
+                    <>
+                      <Flex direction="row">
+                        <img src="cursor.svg" />
+                        <Text>{username}</Text>
+                      </Flex>
+                      <div
+                        className="absolute top-5 left-2 px-4 py-2 bg-blue-500 text-white leading-relaxed text-sm"
+                        onKeyUp={(e) => e.stopPropagation()}
+                        style={{
+                          borderRadius: 20,
+                        }}
+                      >
+                        {state.previousMessage && (
+                          <div>{state.previousMessage}</div>
+                        )}
+                        <textarea
+                          className="bg-transparent border-none	outline-none text-white placeholder-blue-300 w-60"
+                          autoFocus={true}
+                          onChange={(e) => {
+                            updateMyPresence({ message: e.target.value });
+                            setState({
+                              mode: CursorMode.Chat,
+                              previousMessage: null,
+                              message: e.target.value,
+                            });
+                          }}
+                          placeholder={
+                            state.previousMessage ? "" : "Say something…"
+                          }
+                          value={state.message}
+                          maxLength={50}
+                        />
+                      </div>
+                    </>
+                  )}
+                  {state.mode === CursorMode.ReactionSelector && (
+                    <ReactionSelector
+                      setReaction={(reaction) => {
+                        setReaction(reaction);
+                      }}
+                    />
+                  )}
+                  {state.mode === CursorMode.Reaction && (
+                    <div className="absolute top-3.5 left-1 pointer-events-none select-none">
+                      {state.reaction}
+                    </div>
+                  )}
+                </div>
+              )}
+              {others.map(({ connectionId, presence }) => {
+                if (presence == null || !presence.cursor) {
+                  return null;
+                }
 
-              return (
-                <Cursor
-                  key={connectionId}
-                  color={COLORS[connectionId % COLORS.length]}
-                  x={presence.cursor.x}
-                  y={presence.cursor.y}
-                  message={presence.message}
-                  username={presence.username}
-                  address={accountData?.address}
-                />
-              );
-            })}
-          </div>
+                return (
+                  <Cursor
+                    key={connectionId}
+                    color={COLORS[connectionId % COLORS.length]}
+                    x={presence.cursor.x}
+                    y={presence.cursor.y}
+                    message={presence.message}
+                    username={presence.username}
+                    address={accountData?.address}
+                  />
+                );
+              })}
+            </div>
+          ) : (
+            <div
+              style={{
+                height: "100%",
+                width: "100%",
+                cursor:
+                  state.mode === CursorMode.Chat
+                    ? "none"
+                    : "url(cursor.svg) 0 0, auto",
+              }}
+            >
+              {reactions.map((reaction) => {
+                return (
+                  <FlyingReaction
+                    key={reaction.timestamp.toString()}
+                    x={reaction.point.x}
+                    y={reaction.point.y}
+                    timestamp={reaction.timestamp}
+                    value={reaction.value}
+                  />
+                );
+              })}
+              {cursor && (
+                <div
+                  className="absolute top-0 left-0"
+                  style={{
+                    transform: `translateX(${cursor.x}px) translateY(${cursor.y}px)`,
+                  }}
+                >
+                  {state.mode === CursorMode.Hidden && (
+                    <Flex maxW="120px">
+                      <Text noOfLines={4} fontSize="sm" color="black">
+                        click anywhere around the video to comment!
+                      </Text>
+                    </Flex>
+                  )}
+                  {state.mode === CursorMode.Chat && (
+                    <>
+                      <Flex direction="row">
+                        <img src="cursor.svg" />
+                        <Text>{username}</Text>
+                      </Flex>
+                      <div
+                        className="absolute top-5 left-2 px-4 py-2 bg-blue-500 text-white leading-relaxed text-sm"
+                        onKeyUp={(e) => e.stopPropagation()}
+                        style={{
+                          borderRadius: 20,
+                        }}
+                      >
+                        {state.previousMessage && (
+                          <div>{state.previousMessage}</div>
+                        )}
+                        <textarea
+                          className="bg-transparent border-none	outline-none text-white placeholder-blue-300 w-60"
+                          autoFocus={true}
+                          onChange={(e) => {
+                            updateMyPresence({ message: e.target.value });
+                            setState({
+                              mode: CursorMode.Chat,
+                              previousMessage: null,
+                              message: e.target.value,
+                            });
+                          }}
+                          placeholder={
+                            state.previousMessage ? "" : "Say something…"
+                          }
+                          value={state.message}
+                          maxLength={50}
+                        />
+                      </div>
+                    </>
+                  )}
+                  {state.mode === CursorMode.ReactionSelector && (
+                    <ReactionSelector
+                      setReaction={(reaction) => {
+                        setReaction(reaction);
+                      }}
+                    />
+                  )}
+                  {state.mode === CursorMode.Reaction && (
+                    <div className="absolute top-3.5 left-1 pointer-events-none select-none">
+                      {state.reaction}
+                    </div>
+                  )}
+                </div>
+              )}
+              {others.map(({ connectionId, presence }) => {
+                if (presence == null || !presence.cursor) {
+                  return null;
+                }
+
+                return (
+                  <Cursor
+                    key={connectionId}
+                    color={COLORS[connectionId % COLORS.length]}
+                    x={presence.cursor.x}
+                    y={presence.cursor.y}
+                    message={presence.message}
+                    username={presence.username}
+                    address={accountData?.address}
+                  />
+                );
+              })}
+            </div>
+          )}
         </GridItem>
         <GridItem rowSpan={3} colSpan={1}></GridItem>
         <GridItem rowSpan={2} colSpan={1}></GridItem>
