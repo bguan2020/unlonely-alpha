051e7cdc8647bd7af5b3c75fcf64cbe6efd62ccb
Brian Guan <bguan2020@ucla.edu>
2022-07-25 12:57:46 -0700

    font fix

--- unlonely-alpha/web/src/pages/_document.tsx
+++ unlonely-alpha/web/src/pages/_document.tsx
@@ -25,9 +25,9 @@ export default class UnlonelyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          {/* <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-          <link href="https://fonts.googleapis.com/css2?family=Anonymous+Pro&family=DM+Sans&display=swap" rel="stylesheet"/>
+          <link href="https://fonts.googleapis.com/css2?family=Anonymous+Pro&family=DM+Sans&display=swap" rel="stylesheet"/> */}
         </Head>
         <body>
           <ColorModeScript
