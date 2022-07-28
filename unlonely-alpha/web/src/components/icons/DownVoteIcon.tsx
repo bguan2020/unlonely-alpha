1b32f899aadf8675d998c1532676b3ebbfdbc5ff
Brian Guan <bguan2020@ucla.edu>
2022-07-27 17:48:31 -0700

    arrows

--- unlonely-alpha/web/src/components/icons/DownVoteIcon.tsx
+++ unlonely-alpha/web/src/components/icons/DownVoteIcon.tsx
@@ -4,7 +4,7 @@ export const DownVoteIcon = createIcon({
   displayName: "DownVoteIcon",
   viewBox: "175 100 350 350",
   path: (
-    <g fill="white">
+    <g fill="black">
       <path d="m373.38 92.969v284.05l130.93-130.93 32.727 33.906-187.03 187.03-187.03-187.03 32.73-32.727 130.92 129.75v-284.05z" />
     </g>
   ),
--- unlonely-alpha/web/src/components/icons/UpVoteIcon.tsx
+++ unlonely-alpha/web/src/components/icons/UpVoteIcon.tsx
@@ -4,7 +4,7 @@ export const UpVoteIcon = createIcon({
   displayName: "UpVoteIcon",
   viewBox: "175 100 350 350",
   path: (
-    <g fill="white">
+    <g fill="black">
       <path d="m373.38 467.03v-284.05l130.93 130.93 32.727-33.906-187.03-187.03-187.03 187.03 32.73 32.727 130.92-129.75v284.05z" />
     </g>
   ),
