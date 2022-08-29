37dfb69f55d1eee025bc3f98c17459fff828e030
Brian Guan <bguan2020@ucla.edu>
2022-08-28 20:36:39 -0700

    redirects

--- unlonely-alpha/web/next.config.js
+++ unlonely-alpha/web/next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/channels/youtube',
+        source: ['/channels/youtube', '/channels/1'], 
         destination: '/channels/brian',
         permanent: true,
       },
