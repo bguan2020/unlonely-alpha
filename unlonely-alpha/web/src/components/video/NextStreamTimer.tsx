9ff59afb1295e28316b2f71eb92e7e140a39546c
Brian Guan <bguan2020@ucla.edu>
2022-09-21 12:19:15 +0100

    new date

--- unlonely-alpha/web/src/components/video/NextStreamTimer.tsx
+++ unlonely-alpha/web/src/components/video/NextStreamTimer.tsx
@@ -23,7 +23,7 @@ const NextStreamTimer: React.FunctionComponent = () => {
     const now = new Date();
     // next stream time set to july 25, 2022 at 7pm PST, timezone specificed in moment-timezone
     const nextStreamTime = moment.tz(
-      "2022-09-20T19:00:00",
+      "2022-09-26T19:00:00",
       "America/Los_Angeles"
     );
     const diff = nextStreamTime.diff(now, "seconds");
