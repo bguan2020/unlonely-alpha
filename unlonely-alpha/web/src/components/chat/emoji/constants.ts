50df8200f4f111fd18536982f97d41e96d04153d
Brian Guan <bguan2020@ucla.edu>
2022-10-03 18:27:56 -0700

    new gifs

--- unlonely-alpha/web/src/components/chat/emoji/constants.ts
+++ unlonely-alpha/web/src/components/chat/emoji/constants.ts
@@ -11,11 +11,14 @@ export const categories = {
 };
 
 export const gifsList = [
+  "https://i.imgur.com/VXcq6We.gif", // what just happened
+  "https://i.imgur.com/H9cnvfn.gif", // whoo yay grace
+  "https://i.imgur.com/VyPH0Az.gif", // the metaverse
   "https://i.imgur.com/wbUNcyS.gif", // salute
   "https://i.imgur.com/ynQN8Ct.gif", // jeff bezos
-  "https://i.imgur.com/NurjwAK.gif", // bong hit
+  // "https://i.imgur.com/NurjwAK.gif", // bong hit
   "https://i.imgur.com/zTfFgtZ.gif", // dancing
-  "https://i.imgur.com/IW1whk4.gif", // tofu
+  // "https://i.imgur.com/IW1whk4.gif", // tofu
 ];
 
 export const categoriesList = Object.keys(categories);
