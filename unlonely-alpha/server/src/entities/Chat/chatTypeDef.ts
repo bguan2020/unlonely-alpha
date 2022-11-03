1ff6c254d57b775b19d406f6eadc8d66c4339a33
Brian Guan <bguan2020@ucla.edu>
2022-11-03 16:34:31 -0700

    fcbot

--- unlonely-alpha/server/src/entities/Chat/chatTypeDef.ts
+++ unlonely-alpha/server/src/entities/Chat/chatTypeDef.ts
@@ -19,6 +19,7 @@ export const typeDef = gql`
 
   extend type Query {
     firstChatExists: Boolean
+    chatBot: [Chat]
   }
 
   extend type Mutation {
---
+++ unlonely-alpha/server/src/utils/publishCast.ts
@@ -0,0 +1,30 @@
+import {
+  Farcaster,
+  FarcasterGuardianContentHost,
+} from "@standard-crypto/farcaster-js";
+import { Wallet } from "ethers";
+import wallet from "./wallet";
+
+/**
+ * Credit: https://gist.github.com/gskril/59d16fefbc411e61c9cce41963f3accf
+ * MODIFIED FROM STANDARD-CRYPTO LIBRARY
+ * Signs and publishes a simple text string.
+ * The cast will be attributed to the username currently registered
+ * to the given private key's address.
+ */
+const replyTo = "0x6907409323e89c584807dc44ba6e6ed3fac0aeae7bb3aebe7921dcef3ad854f6"; // INSERT HERE
+const _defaultFarcaster = new Farcaster()
+export default async function publishCast(text: string) { 
+  const contentHost = new FarcasterGuardianContentHost(wallet.privateKey);
+  const signer = new Wallet(wallet.privateKey);
+  const unsignedCast = await _defaultFarcaster.prepareCast({
+    fromUsername: "briang",
+    text,
+    replyTo,
+  });
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const signedCast = await Farcaster.signCast(unsignedCast, signer);
+  await contentHost.publishCast(signedCast);
+  return signedCast;
+}
