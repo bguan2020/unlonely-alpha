33403bf4ccaf382fb549470db17f84447b7708fd
Brian Guan <bguan2020@ucla.edu>
2022-11-08 21:09:58 -0800

    new cast

--- unlonely-alpha/server/src/utils/publishCast.ts
+++ unlonely-alpha/server/src/utils/publishCast.ts
@@ -12,7 +12,7 @@ import wallet from "./wallet";
  * The cast will be attributed to the username currently registered
  * to the given private key's address.
  */
-const replyTo = "0x685eecd96db134f211271f3a818579a3471e3ef7b3731ec2ae2fff896f376616"; // INSERT HERE
+const replyTo = "0x70f0891364c2200291cc63765ec486e314efa49469653aa873089ec515935332"; // INSERT HERE
 const _defaultFarcaster = new Farcaster()
 export default async function publishCast(text: string) { 
   const contentHost = new FarcasterGuardianContentHost(wallet.privateKey);
