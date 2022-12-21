0eb76f98aa8fae60b7e195164b22ddd4388b80bf
pugson <hi@wojtek.im>
2022-12-21 03:02:08 +0100

    wip

--- unlonely-alpha-mobile/app/(screens)/live.tsx
+++ unlonely-alpha-mobile/app/(screens)/live.tsx
@@ -1,18 +1,49 @@
-import { Dimensions, Pressable, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, useWindowDimensions, View } from 'react-native';
 import IVSPlayer, { IVSPlayerRef } from 'amazon-ivs-react-native-player';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { statusBarHeight } from '../../utils/statusbar';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
+import { easeGradient } from 'react-native-easing-gradient';
+import { WebView } from 'react-native-webview';
+
+const { colors, locations } = easeGradient({
+  // Eased gradient function so it doesn’t look like garbage
+  colorStops: {
+    0: {
+      color: 'rgba(0, 0, 0, 1)',
+    },
+    1: {
+      color: 'rgba(0, 0, 0, 0.5)',
+    },
+  },
+});
 
 export default function LiveScreen() {
+  const { width } = useWindowDimensions();
   const mediaPlayerRef = useRef<IVSPlayerRef>(null);
+  const webViewRef = useRef<WebView>(null);
 
   const togglePip = () => {
     console.log('toggling pip');
     mediaPlayerRef.current?.togglePip();
   };
 
+  const handlePlay = () => {
+    mediaPlayerRef.current?.play();
+  };
+
   const handlePause = () => {
     mediaPlayerRef.current?.pause();
+  };
+
+  const catchWebViewNavigationStateChange = (newNavState: any) => {
+    const { url } = newNavState;
+
+    if (url !== 'https://www.unlonely.app/channels/brian') {
+      webViewRef.current.stopLoading();
+      webViewRef.current.reload();
+    }
   };
 
   return (
@@ -22,12 +53,12 @@ export default function LiveScreen() {
           styles.videoContainer,
           {
             width: '100%',
-            height: Dimensions.get('window').width * (9 / 16),
+            height: Math.round(width * (9 / 16)),
           },
         ]}
       >
         <IVSPlayer
-          style={styles.video}
+          style={styles.videoPlayer}
           resizeMode={'aspectFit'}
           streamUrl={
             'https://0ef8576db087.us-west-2.playback.live-video.net/api/video/v1/us-west-2.500434899882.channel.8e2oKm7LXNGq.m3u8'
@@ -35,18 +66,54 @@ export default function LiveScreen() {
           onLiveLatencyChange={latency => {
             console.log('live latency: ', latency);
           }}
-          autoplay={true}
+          autoplay={false}
           liveLowLatency={true}
           ref={mediaPlayerRef}
         />
+        <View style={styles.videoOverlay}>
+          <LinearGradient
+            colors={colors}
+            locations={locations}
+            start={[0, 1]}
+            end={[0, 0]}
+            style={{
+              width: '100%',
+              height: '100%',
+              position: 'absolute',
+            }}
+          />
+          <MaterialCommunityIcons name="antenna" size={64} color="white" />
+          <Text style={styles.videoOverlayText}>loading live stream...</Text>
+        </View>
       </View>
-      <Pressable onPress={togglePip}>
-        <Text style={styles.pip}>Toggle PiP</Text>
-      </Pressable>
+      <View style={styles.bar}>
+        <Pressable onPress={togglePip}>
+          <Text style={styles.pip}>Toggle PiP</Text>
+        </Pressable>
 
-      <Pressable onPress={handlePause}>
-        <Text style={styles.pip}>Pause</Text>
-      </Pressable>
+        <Pressable onPress={handlePlay}>
+          <Text style={styles.pip}>Play</Text>
+        </Pressable>
+
+        <Pressable onPress={handlePause}>
+          <Text style={styles.pip}>Pause</Text>
+        </Pressable>
+      </View>
+      <WebView
+        ref={webViewRef}
+        source={{ uri: 'https://www.unlonely.app/channels/brian' }}
+        onNavigationStateChange={catchWebViewNavigationStateChange}
+        style={{
+          paddingBottom: 100,
+        }}
+      />
+      <View
+        // this is a hack to make the webview not go under the tab bar
+        style={{
+          backgroundColor: 'black',
+          height: 80,
+        }}
+      />
     </View>
   );
 }
@@ -54,20 +121,36 @@ export default function LiveScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ccc',
-    paddingTop: statusBarHeight,
+    backgroundColor: 'black',
+    paddingTop: statusBarHeight - 2,
   },
   videoContainer: {
-    // height: 400,
-    backgroundColor: 'red',
+    backgroundColor: 'hsl(0, 0%, 2%)',
   },
-  video: {
-    // width: '100%',
-    // height: '100%',
+  videoPlayer: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+  },
+  videoOverlay: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+  },
+  videoOverlayText: {
+    color: 'white',
+    fontSize: 18,
+    fontFamily: 'NeuePixelSans',
+    paddingTop: 8,
+  },
+  bar: {
+    flexDirection: 'row',
+    backgroundColor: 'blue',
+    justifyContent: 'center',
   },
   pip: {
-    fontSize: 20,
-    color: 'black',
-    margin: 20,
+    fontSize: 16,
+    color: 'white',
+    padding: 8,
   },
 });
