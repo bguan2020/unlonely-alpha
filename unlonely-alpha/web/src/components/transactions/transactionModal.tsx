02645599e2ca4278aac4c1d8fb14221d51ff4c3f
Brian Guan <bguan2020@ucla.edu>
2023-01-03 12:27:01 -0800

    some fixes but still need to solve approve

--- unlonely-alpha/web/src/components/transactions/transactionModal.tsx
+++ unlonely-alpha/web/src/components/transactions/transactionModal.tsx
@@ -25,6 +25,7 @@ import { BRIAN_TOKEN_ADDRESS, ETHEREUM_MAINNET_CHAIN_ID } from "../../constants"
 import BrianToken from "../../utils/newsToken.json";
 import { CustomToast } from "../general/CustomToast";
 import { useUser } from "../../hooks/useUser";
+
 type Props = {
   onSuccess: (hash: string) => void;
   price: number;
@@ -34,9 +35,11 @@ type Props = {
 export default function TransactionModal({ onSuccess, price, title }: Props) {
   const { user } = useUser();
   const { addToast } = CustomToast();
-  const [open, setOpen] = useState(false);
-  const [step, setStep] = useState(0);
+  const [open, setOpen] = useState<boolean>(false);
+  const [step, setStep] = useState<number>(0);
+  const [allowanceState, setAllowanceState] = useState<Number>(0);
   const accountData = useAccount();
+
   const {
     data: data3,
     error: error3,
@@ -57,10 +60,41 @@ export default function TransactionModal({ onSuccess, price, title }: Props) {
     addressOrName: BRIAN_TOKEN_ADDRESS,
     contractInterface: BrianToken,
     functionName: "allowance",
-    args: [accountData?.address],
+    args: [accountData?.address, BRIAN_TOKEN_ADDRESS],
     chainId: ETHEREUM_MAINNET_CHAIN_ID,
+    onSuccess: (data) => {
+      setAllowanceState(Number(ethers.utils.formatEther(data)));
+      console.log("allowanceState on Success", allowanceState);
+    },
   });
 
+  // step 1: approval
+  const { config: approvalConfig } = usePrepareContractWrite({
+    addressOrName: BRIAN_TOKEN_ADDRESS,
+    contractInterface: BrianToken,
+    functionName: "approve",
+    args: [accountData?.address, price*20000],
+    enabled: Boolean(price),
+    onError: (err) => {
+      setStep(0);
+    },
+  });
+
+  const { data: approvalData, error: approvalError, writeAsync: writeApproval } = useContractWrite(approvalConfig);
+
+  const { isLoading, isSuccess } = useWaitForTransaction({
+    hash: approvalData?.hash,
+    onError: (err) => {
+      setStep(0);
+    },
+    onSuccess: () => {
+      const newAllowance = (allowanceState as number)+(price*20);
+      setAllowanceState(newAllowance);
+      console.log("new allowance", newAllowance);
+    },
+  });
+
+  // step 2: transfer
   const { config: transferConfig } = usePrepareContractWrite({
     addressOrName: BRIAN_TOKEN_ADDRESS,
     contractInterface: BrianToken,
@@ -71,33 +105,10 @@ export default function TransactionModal({ onSuccess, price, title }: Props) {
       setStep(0);
     },
   });
-  const { data: transferData, writeAsync: transferWrite } =
-    useContractWrite(transferConfig);
-  const { config } = usePrepareContractWrite({
-    addressOrName: BRIAN_TOKEN_ADDRESS,
-    contractInterface: BrianToken,
-    functionName: "approve",
-    args: [accountData?.address, price],
-    enabled: Boolean(price),
-    onSuccess: async (data) => {
-      try {
-        transferWrite && (await transferWrite());
-      } catch (e) {
-        setStep(0);
-      }
-    },
-    onError: (err) => {
-      setStep(0);
-    },
-  });
 
-  const { data, error, writeAsync } = useContractWrite(config);
-  const { isLoading, isSuccess } = useWaitForTransaction({
-    hash: data?.hash,
-    onError: (err) => {
-      setStep(0);
-    },
-  });
+  const { data: transferData, writeAsync: transferWrite, error: transferRejectedError } =
+    useContractWrite(transferConfig);
+  
   const {
     isLoading: transferLoading,
     isSuccess: isTransferSuccess,
@@ -114,28 +125,31 @@ export default function TransactionModal({ onSuccess, price, title }: Props) {
       setStep(1);
       transferWrite && transferWrite();
     }
-  }, [isSuccess]);
+  }, [isSuccess, transferRejectedError]);
+
 
   useEffect(() => {
-    if (data3) {
-      console.log(Math.round(Number(ethers.utils.formatEther(data3))), "balanceOf");
-    }
-  }, [data3]);
-
-  useEffect(() => {
+    console.log("isTransferSuccess", isTransferSuccess);
     if (isTransferSuccess) {
       setOpen(false);
       setStep(0);
       onSuccess && onSuccess(transferData?.hash as string);
     }
   }, [isTransferSuccess]);
-  const handleTransaction = async (price: number, allowance: number) => {
+
+  useEffect(() => {
+    console.log("allowance", allowance);
+    console.log("allowanceLoading", allowanceLoading);
+    console.log("allowanceError", allowanceError);
+  }, [allowanceLoading, allowance, allowanceError]);
+
+  const handleTransaction = async (price: number) => {
     try {
-      if (allowance >= price) {
+      if (allowanceState >= price) {
         setStep(1);
         transferWrite && (await transferWrite());
       } else {
-        writeAsync && (await writeAsync());
+        writeApproval && (await writeApproval());
       }
     } catch (e) {
       setStep(0);
@@ -191,7 +205,6 @@ export default function TransactionModal({ onSuccess, price, title }: Props) {
                     try {
                       await handleTransaction(
                         price,
-                        allowance ? parseInt(allowance.toString()) : 0
                       );
                     } catch (e) {
                       setStep(0);
