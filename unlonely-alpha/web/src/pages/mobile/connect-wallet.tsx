52070f92589f8c5195ab5b8f53ee7773de104920
pugson <hi@wojtek.im>
2023-01-08 23:22:28 +0100

    connectkit mobile prep

--- unlonely-alpha/web/src/pages/mobile/connect-wallet.tsx
+++ unlonely-alpha/web/src/pages/mobile/connect-wallet.tsx
@@ -1,7 +1,7 @@
 import { Box, Flex } from "@chakra-ui/react";
 import { useModal } from "connectkit";
 import { useEffect } from "react";
-import { useAccount } from "wagmi";
+import { useAccount, useEnsAvatar, useEnsName } from "wagmi";
 import ConnectWallet from "../../components/navigation/ConnectKit";
 
 const styles = `
@@ -33,12 +33,12 @@ const styles = `
 export default function MobileConnectWallet() {
   const { open, setOpen } = useModal();
   const account = useAccount({
-    onConnect({ address }) {
+    onConnect() {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       if (window.ReactNativeWebView !== undefined) {
         // @ts-ignore
-        window.ReactNativeWebView.postMessage(address);
+        window.ReactNativeWebView.postMessage("wallet_connected");
       }
     },
     onDisconnect() {
@@ -50,6 +50,25 @@ export default function MobileConnectWallet() {
       }
     },
   });
+  const { data: ensName } = useEnsName({ address: account.address });
+  const { data: ensAvatar } = useEnsAvatar({ address: account.address });
+
+  useEffect(() => {
+    const walletData = {
+      address: account.address || null,
+      ensName: ensName || null,
+      ensAvatar: ensAvatar || null,
+    };
+
+    if (walletData.address) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      if (window.ReactNativeWebView !== undefined) {
+        // @ts-ignore
+        window.ReactNativeWebView.postMessage(JSON.stringify(walletData));
+      }
+    }
+  }, [account, ensName, ensAvatar]);
 
   useEffect(() => {
     setOpen(true);
