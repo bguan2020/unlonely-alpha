af66e0354274c9379d828266ad2347d83c4a8d5e
Brian Guan <bguan2020@ucla.edu>
2023-01-17 09:39:47 -0800

    fix

--- unlonely-alpha/server/src/utils/user.ts
+++ unlonely-alpha/server/src/utils/user.ts
@@ -12,49 +12,31 @@ export const findOrCreateUser = async ({ address }: { address: string }) => {
     },
   });
 
-  if (user?.isLensUser === false) {
-    const { data } = await lensClient.query({
-      query: LENS_GET_DEFAULT_PROFILE,
-      variables: {
-        ethereumAddress: address,
-      },
-    });
-
-    if (data && data.defaultProfile) {
-      user = await prisma.user.update({
-        where: {
-          address: address,
-        },
-        data: {
-          isLensUser: true,
-          lensHandle: data.defaultProfile.handle,
-          lensImageUrl: data.defaultProfile.picture.original.url,
+  if (!user) {
+    try {
+      const username = await getEnsName(address);
+  
+      const { data } = await lensClient.query({
+        query: LENS_GET_DEFAULT_PROFILE,
+        variables: {
+          ethereumAddress: address,
         },
       });
+  
+      user = await prisma.user.create({
+        data: {
+          address: address,
+          username: username,
+          isLensUser: data && data.defaultProfile ? true : false,
+          lensHandle: data && data.defaultProfile ? data.defaultProfile.handle : "",
+          lensImageUrl: data && data.defaultProfile
+            ? data.defaultProfile.picture.original.url
+            : "",
+        },
+      });
+    } catch (e) {
+      console.log(e);
     }
-  }
-
-  if (!user) {
-    const username = await getEnsName(address);
-
-    const { data } = await lensClient.query({
-      query: LENS_GET_DEFAULT_PROFILE,
-      variables: {
-        ethereumAddress: address,
-      },
-    });
-
-    user = await prisma.user.create({
-      data: {
-        address: address,
-        username: username,
-        isLensUser: data && data.defaultProfile ? true : false,
-        lensHandle: data && data.defaultProfile ? data.defaultProfile.handle : "",
-        lensImageUrl: data && data.defaultProfile
-          ? data.defaultProfile.picture.original.url
-          : "",
-      },
-    });
   }
 
   return user;
