1dd9445f0b1dbddfc02fc1029cdd863efc8767d4
pugson <hi@wojtek.im>
2023-01-30 14:14:08 +0100

    fix name

--- unlonely-alpha/web/src/pages/mobile/notifications.tsx
+++ unlonely-alpha/web/src/pages/mobile/notifications.tsx
@@ -329,7 +329,7 @@ export default function MobileNotifications() {
                   if (index === 0) {
                     setSelectedType("live");
                   } else {
-                    setSelectedType("nfcs");
+                    setSelectedType("nfc");
                   }
                 }}
               >
