9e7cd1e2847121a7e5d3a1708335a505819ed72a
pugson <hi@wojtek.im>
2023-04-13 22:41:37 +0200

    coinbase wallet mobile session cloning

---
+++ unlonely-alpha/web/src/pages/mobile/coinbase-paste.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import ConnectWallet from "../../components/navigation/ConnectKit";
+import { Box } from "@chakra-ui/react";
+import { useClipboard } from "use-clipboard-copy";
+import { useAccount } from "wagmi";
+
+export default function MobileCoinbasePaste() {
+  const [done, setDone] = useState(false);
+
+  const handlePaste = async () => {
+    const text = await navigator.clipboard.readText();
+
+    const localStorageObj = JSON.parse(text);
+    for (const key in localStorageObj) {
+      if (localStorageObj.hasOwnProperty(key)) {
+        localStorage.setItem(key, localStorageObj[key]);
+      }
+    }
+
+    setDone(true);
+  };
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100svh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        padding: 20,
+      }}
+    >
+      <div>
+        {!done && (
+          <Box marginTop={10}>
+            <button
+              onClick={handlePaste}
+              style={{
+                padding: "10px 20px",
+                backgroundColor: "white",
+                borderRadius: 12,
+              }}
+            >
+              paste session
+            </button>
+          </Box>
+        )}
+        {done && <p>all done. you can close this window now.</p>}
+      </div>
+    </div>
+  );
+}
