eaf8f333883a8897cbc93a04d1c85090bbf11169
Brian Guan <bguan2020@ucla.edu>
2023-05-02 11:21:40 -0700

    new thread

--- unlonely-alpha/server/src/utils/farcaster/threadReply.js
+++ unlonely-alpha/server/src/utils/farcaster/threadReply.js
@@ -4,7 +4,7 @@ const fetch = require("node-fetch");
 const { MerkleAPIClient } = require("@standard-crypto/farcaster-js");
 const PrismaClient = require("@prisma/client").PrismaClient;
 
-const parentCastHash = "0x4f2076cdbd5056b3207a3883954f5d37c087011e";
+const parentCastHash = "0x14b42bc954efc080ecd1ad333f245e5c72926e55";
 
 // connect to database
 const client = new Client({
