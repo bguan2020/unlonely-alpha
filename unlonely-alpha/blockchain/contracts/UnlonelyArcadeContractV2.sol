cee03a5189175b84e6885fed5162924c004d212c
Brian Guan <bguan2020@ucla.edu>
2023-06-05 01:28:59 -0700

    better contract

---
+++ unlonely-alpha/blockchain/contracts/UnlonelyArcadeContractV2.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract UnlonelyArcadeContract {
+    using SafeERC20 for IERC20;
+
+    address public brian;
+    mapping(address => IERC20) public creatorTokens;
+    mapping(address => uint256) public tokenPrices;
+
+    constructor() {
+        brian = msg.sender;
+    }
+
+    modifier onlyBrian() {
+        require(msg.sender == brian, "Only Brian can call this function.");
+        _;
+    }
+
+    function addCreatorToken(address _creatorToken, uint256 _initialPrice) external onlyBrian {
+        require(_initialPrice > 0, "Token price must be greater than zero.");
+        require(creatorTokens[_creatorToken] == IERC20(address(0)), "Token already exists.");
+
+        creatorTokens[_creatorToken] = IERC20(_creatorToken);
+        tokenPrices[_creatorToken] = _initialPrice;
+    }
+
+    function setTokenPrice(address _creatorToken, uint256 _newPrice) external onlyBrian {
+        require(_newPrice > 0, "Token price must be greater than zero.");
+        require(creatorTokens[_creatorToken] != IERC20(address(0)), "Token does not exist.");
+
+        tokenPrices[_creatorToken] = _newPrice;
+    }
+
+    function buyCreatorToken(address _creatorToken, uint256 tokenAmount) payable external {
+        require(creatorTokens[_creatorToken] != IERC20(address(0)), "Token does not exist.");
+
+        // Calculate required ETH amount
+        uint256 ethAmount = calculateEthAmount(_creatorToken, tokenAmount);
+
+        require(msg.value >= ethAmount, "Insufficient Ether sent.");
+
+        // Send ETH to Brian
+        (bool sent,) = payable(brian).call{value: msg.value}("");
+        require(sent, "Failed to transfer Ether");
+
+        // Transfer CreatorToken to the buyer
+        creatorTokens[_creatorToken].safeTransferFrom(brian, msg.sender, tokenAmount);
+    }
+
+    function useFeature(address _creatorToken, uint256 _featurePrice) external {
+        require(creatorTokens[_creatorToken] != IERC20(address(0)), "Token does not exist.");
+
+        // Check if the user has enough tokens
+        require(creatorTokens[_creatorToken].balanceOf(msg.sender) >= _featurePrice, "Insufficient CreatorToken balance");
+
+        // Transfer tokens from the user to Brian
+        creatorTokens[_creatorToken].safeTransferFrom(msg.sender, brian, _featurePrice);
+
+        // Implement the actual feature usage logic here
+    }
+
+    function calculateEthAmount(address _creatorToken, uint256 tokenAmount) public view returns (uint256) {
+        require(creatorTokens[_creatorToken] != IERC20(address(0)), "Token does not exist.");
+
+        return tokenAmount / tokenPrices[_creatorToken];
+    }
+}
