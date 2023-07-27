f156f2a32b0a204283d20bd798442f825063ae48
Danny Fung <dannyfung97@gmail.com>
2023-07-27 13:20:02 -0700

    apiClient hook fix

--- unlonely-alpha/web/src/apiClient/hooks.ts
+++ unlonely-alpha/web/src/apiClient/hooks.ts
@@ -62,6 +62,7 @@ export const useAuthedMutation = <
       setError({
         message,
         graphQLErrors: [],
+        protocolErrors: [],
         clientErrors: [],
         networkError: null,
         extraInfo: {},
