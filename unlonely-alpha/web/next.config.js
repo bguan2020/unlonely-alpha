4498e00483c8f571d66d9ae3c784930606722cd7
Danny Fung <dannyfung97@gmail.com>
2023-08-17 11:22:05 -0700

    reverted next.config.js

--- unlonely-alpha/web/next.config.js
+++ unlonely-alpha/web/next.config.js
@@ -1,12 +1,30 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const withPWA = require("next-pwa")({
-  dest: "public", // this is where the service worker will check for assets to cache
-  disable: process.env.NODE_ENV !== "production",
-  skipWaiting: true,
-});
-/* eslint-enable @typescript-eslint/no-var-requires */
+// /* eslint-disable @typescript-eslint/no-var-requires */
+// const withPWA = require("next-pwa")({
+//   dest: "public", // this is where the service worker will check for assets to cache
+//   disable: process.env.NODE_ENV !== "production",
+//   skipWaiting: true,
+// });
+// /* eslint-enable @typescript-eslint/no-var-requires */
 
-module.exports = withPWA({
+// module.exports = withPWA({
+//   async redirects() {
+//     return [
+//       {
+//         source: "/channels/youtube",
+//         destination: "/channels/brian",
+//         permanent: true,
+//       },
+//       {
+//         source: "/channels/1",
+//         destination: "/channels/brian",
+//         permanent: true,
+//       },
+//     ];
+//   },
+//   // any other configurations you have...
+// });
+
+module.exports = {
   async redirects() {
     return [
       {
@@ -21,5 +39,4 @@ module.exports = withPWA({
       },
     ];
   },
-  // any other configurations you have...
-});
+};
