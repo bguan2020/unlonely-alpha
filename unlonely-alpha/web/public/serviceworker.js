d982c53d924cb607b18a4125f5c6cedba5dd3c8f
Brian Guan <bguan2020@ucla.edu>
2023-08-17 13:31:47 -0400

    commit

--- unlonely-alpha/web/public/serviceworker.js
+++ unlonely-alpha/web/public/serviceworker.js
@@ -1,7 +1,9 @@
 self.addEventListener('push', function(event) {
+  console.log("Push notification received", event);
 
   var myNotif = event.data.json();
   const promiseChain = self.registration.showNotification(myNotif.title);
+  console.log(promiseChain);
 
   event.waitUntil(promiseChain);
 });
