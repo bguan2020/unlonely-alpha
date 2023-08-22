7e55256fc7b6b76a718f5c3488ed9680abb818bd
Danny Fung <dannyfung97@gmail.com>
2023-08-22 11:27:22 -0700

    tiny change

--- unlonely-alpha/web/src/components/general/mobile-prompts/AddToIosSafari.tsx
+++ unlonely-alpha/web/src/components/general/mobile-prompts/AddToIosSafari.tsx
@@ -44,8 +44,8 @@ export default function AddToIosSafari(props: Props) {
           </Text>
         </Flex>
         <Text textAlign={"center"} color="black" fontSize="12px" mt="10px">
-          (Can't find the "add to home screen" button? make sure you're in an
-          in-app browser!)
+          (Can't find the "add to home screen" button? make sure you're not in
+          an in-app browser!)
         </Text>
       </Flex>
     </TransactionModalTemplate>
--- unlonely-alpha/web/src/components/general/mobile-prompts/AddToMobileFirefoxIos.tsx
+++ unlonely-alpha/web/src/components/general/mobile-prompts/AddToMobileFirefoxIos.tsx
@@ -48,8 +48,8 @@ export default function AddToMobileFirefoxIos(props: Props) {
           </Text>
         </Flex>
         <Text textAlign={"center"} color="black" fontSize="12px" mt="10px">
-          (Can't find the "add to home screen" button? make sure you're in an
-          in-app browser!)
+          (Can't find the "add to home screen" button? make sure you're not in
+          an in-app browser!)
         </Text>
       </Flex>
     </TransactionModalTemplate>
