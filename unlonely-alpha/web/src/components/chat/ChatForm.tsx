97c9a94f5b2e02f8db5681ef394aa0fde02e8cd7
Danny Fung <dannyfung97@gmail.com>
2023-11-06 17:04:35 -0800

    removed token from chat form

--- unlonely-alpha/web/src/components/chat/ChatForm.tsx
+++ unlonely-alpha/web/src/components/chat/ChatForm.tsx
@@ -14,15 +14,7 @@ import {
   PopoverContent,
   PopoverArrow,
 } from "@chakra-ui/react";
-import React, {
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from "react";
-import Link from "next/link";
-import { parseUnits } from "viem";
+import React, { useCallback, useState } from "react";
 import copy from "copy-to-clipboard";
 import { BsFillHeartFill } from "react-icons/bs";
 
@@ -30,23 +22,16 @@ import {
   ADD_REACTION_EVENT,
   CommandData,
   InteractionType,
-  USER_APPROVAL_AMOUNT,
 } from "../../constants";
 import Commands from "./Commands";
 import { EmojiType, SenderStatus } from "../../constants/types/chat";
 import { useChannelContext } from "../../hooks/context/useChannel";
 import { useUser } from "../../hooks/context/useUser";
 import EmojiButton from "./emoji/EmojiButton";
-import { useApproval } from "../../hooks/contracts/useApproval";
-import { getContractFromNetwork } from "../../utils/contract";
-import CreatorTokenAbi from "../../constants/abi/CreatorToken.json";
-import { formatIncompleteNumber } from "../../utils/validation/input";
-import { useUseFeature } from "../../hooks/contracts/useArcadeContract";
 import centerEllipses from "../../utils/centerEllipses";
 import ConnectWallet from "../navigation/ConnectWallet";
 import useUserAgent from "../../hooks/internal/useUserAgent";
 import { ChatClip } from "./ChatClip";
-import { useNetworkContext } from "../../hooks/context/useNetwork";
 import { REACTION_EMOJIS } from "./emoji/constants";
 import EmojiDisplay from "./emoji/EmojiDisplay";
 
@@ -75,13 +60,10 @@ const ChatForm = ({
 }: Props) => {
   const { user, walletIsConnected, userAddress: address } = useUser();
   const { isStandalone } = useUserAgent();
-  const { network } = useNetworkContext();
-  const { matchingChain, localNetwork, explorerUrl } = network;
 
   const toast = useToast();
   const {
     channel: channelContext,
-    token,
     chat,
     arcade,
     leaderboard,
@@ -92,182 +74,17 @@ const ChatForm = ({
   const { handleIsClipUiOpen, loading: clipLoading } = clipping;
 
   const { channelQueryData } = channelContext;
-  const { userTokenBalance, refetchUserTokenBalance } = token;
+  // const { userTokenBalance, refetchUserTokenBalance } = token;
 
   const [messageText, setMessageText] = useState<string>("");
   const [commandsOpen, setCommandsOpen] = useState(false);
-  const [txTransition, setTxTransition] = useState(false);
   const [error, setError] = useState<string>("");
-  const [tooltipError, setTooltipError] = useState<string>("");
-  const [gifInTransaction, setGifInTransaction] = useState<string>("");
-  const transitioningTxFromApprovalToExec = useRef(false);
 
   const [blastMode, setBlastMode] = useState(false);
-
-  const contract = getContractFromNetwork("unlonelyArcade", localNetwork);
 
   const [showEmojiReactionList, setShowEmojiReactionList] = useState(false);
   const [reactionDisabled, setReactionDisabled] = useState<boolean>(false);
   const isOwner = address === channelQueryData?.owner.address;
-
-  const {
-    requiresApproval,
-    writeApproval,
-    isTxLoading: isApprovalLoading,
-    isTxSuccess: isApprovalSuccess,
-    refetchAllowance,
-  } = useApproval(
-    channelQueryData?.token?.address as `0x${string}`,
-    CreatorTokenAbi,
-    user?.address as `0x${string}`,
-    contract?.address as `0x${string}`,
-    contract?.chainId as number,
-    parseUnits(PRICE as `${number}`, 18),
-    parseUnits(USER_APPROVAL_AMOUNT, 18),
-    {
-      onWriteSuccess: (data) => {
-        toast({
-          render: () => (
-            <Box as="button" borderRadius="md" bg="#287ab0" px={4} h={8}>
-              <Link
-                target="_blank"
-                href={`${explorerUrl}/tx/${data.hash}`}
-                passHref
-              >
-                approve pending, click to view
-              </Link>
-            </Box>
-          ),
-          duration: 9000,
-          isClosable: true,
-          position: "top-right",
-        });
-      },
-      onTxSuccess: (data) => {
-        toast({
-          render: () => (
-            <Box as="button" borderRadius="md" bg="#50C878" px={4} h={8}>
-              <Link
-                target="_blank"
-                href={`${explorerUrl}/tx/${data.transactionHash}`}
-                passHref
-              >
-                approve success, click to view
-              </Link>
-            </Box>
-          ),
-          duration: 9000,
-          isClosable: true,
-          position: "top-right",
-        });
-        useFeature?.();
-        refetchAllowance();
-      },
-    }
-  );
-
-  const tokenAmount_bigint = useMemo(
-    () =>
-      requiresApproval
-        ? BigInt(0)
-        : parseUnits(formatIncompleteNumber(PRICE) as `${number}`, 18),
-    [requiresApproval]
-  );
-
-  const { useFeature, useFeatureTxLoading } = useUseFeature(
-    {
-      creatorTokenAddress: channelQueryData?.token?.address as `0x${string}`,
-      featurePrice: tokenAmount_bigint,
-    },
-    contract,
-    {
-      onWriteSuccess: (data) => {
-        toast({
-          render: () => (
-            <Box as="button" borderRadius="md" bg="#287ab0" px={4} h={8}>
-              <Link
-                target="_blank"
-                href={`${explorerUrl}/tx/${data.hash}`}
-                passHref
-              >
-                useFeature pending, click to view
-              </Link>
-            </Box>
-          ),
-          duration: 9000,
-          isClosable: true,
-          position: "top-right",
-        });
-        setTxTransition(false);
-      },
-      onWriteError: (error) => {
-        toast({
-          duration: 9000,
-          isClosable: true,
-          position: "top-right",
-          render: () => (
-            <Box as="button" borderRadius="md" bg="#bd711b" px={4} h={8}>
-              useFeature cancelled
-            </Box>
-          ),
-        });
-        setTxTransition(false);
-        setError(error);
-      },
-      onTxSuccess: (data) => {
-        toast({
-          render: () => (
-            <Box as="button" borderRadius="md" bg="#50C878" px={4} h={8}>
-              <Link
-                target="_blank"
-                href={`${explorerUrl}/tx/${data.transactionHash}`}
-                passHref
-              >
-                useFeature success, click to view
-              </Link>
-            </Box>
-          ),
-          duration: 9000,
-          isClosable: true,
-          position: "top-right",
-        });
-        sendChatMessage(
-          gifInTransaction !== ""
-            ? gifInTransaction
-            : messageText.replace(/^\s*\n|\n\s*$/g, ""),
-          gifInTransaction !== "",
-          isVipChat ? SenderStatus.VIP : SenderStatus.USER,
-          `${InteractionType.BLAST}:`
-        );
-        setGifInTransaction("");
-        setTxTransition(false);
-        setBlastMode(false);
-        refetchUserTokenBalance?.();
-        refetchAllowance();
-        setMessageText("");
-      },
-      onTxError: (error) => {
-        toast({
-          render: () => (
-            <Box as="button" borderRadius="md" bg="#b82929" px={4} h={8}>
-              useFeature error
-            </Box>
-          ),
-          duration: 9000,
-          isClosable: true,
-          position: "top-right",
-        });
-        setBlastMode(false);
-        setGifInTransaction("");
-        setTxTransition(false);
-        setError(error);
-      },
-    }
-  );
-
-  const txLoading = useMemo(() => {
-    return isApprovalLoading || useFeatureTxLoading;
-  }, [isApprovalLoading, useFeatureTxLoading]);
 
   const messageTextIsEmpty =
     messageText.trim().length === 0 || messageText.trim() === "";
@@ -283,27 +100,16 @@ const ChatForm = ({
         true,
         isVipChat ? SenderStatus.VIP : SenderStatus.USER
       );
-      setMessageText("");
     } else {
-      if (channelQueryData?.token?.address) {
-        if (tooltipError !== "") return;
-        setTxTransition(true);
-        setGifInTransaction(gif);
-        if (requiresApproval && writeApproval) {
-          writeApproval();
-        } else {
-          useFeature?.();
-        }
-      } else {
-        sendChatMessage(
-          gif,
-          true,
-          isVipChat ? SenderStatus.VIP : SenderStatus.USER,
-          `${InteractionType.BLAST}:`
-        );
-        setBlastMode(false);
-      }
+      sendChatMessage(
+        gif,
+        true,
+        isVipChat ? SenderStatus.VIP : SenderStatus.USER,
+        `${InteractionType.BLAST}:`
+      );
+      setBlastMode(false);
     }
+    setMessageText("");
   };
 
   const handleKeyPress = useCallback(
@@ -324,38 +130,18 @@ const ChatForm = ({
           false,
           isVipChat ? SenderStatus.VIP : SenderStatus.USER
         );
-        setMessageText("");
       } else {
-        if (channelQueryData?.token?.address) {
-          if (tooltipError !== "") return;
-          setTxTransition(true);
-          setGifInTransaction("");
-          if (requiresApproval && writeApproval) {
-            writeApproval();
-          } else {
-            useFeature?.();
-          }
-        } else {
-          sendChatMessage(
-            messageText.replace(/^\s*\n|\n\s*$/g, ""),
-            false,
-            isVipChat ? SenderStatus.VIP : SenderStatus.USER,
-            `${InteractionType.BLAST}:`
-          );
-          setBlastMode(false);
-          setMessageText("");
-        }
+        sendChatMessage(
+          messageText.replace(/^\s*\n|\n\s*$/g, ""),
+          false,
+          isVipChat ? SenderStatus.VIP : SenderStatus.USER,
+          `${InteractionType.BLAST}:`
+        );
+        setBlastMode(false);
       }
+      setMessageText("");
     },
-    [
-      blastMode,
-      messageText,
-      requiresApproval,
-      writeApproval,
-      useFeature,
-      messageTextIsEmpty,
-      tooltipError,
-    ]
+    [blastMode, messageText, messageTextIsEmpty]
   );
 
   const handleFormSubmission = useCallback(
@@ -367,59 +153,19 @@ const ChatForm = ({
           false,
           isVipChat ? SenderStatus.VIP : SenderStatus.USER
         );
-        setMessageText("");
       } else {
-        if (channelQueryData?.token?.address) {
-          setTxTransition(true);
-          setGifInTransaction("");
-          if (requiresApproval && writeApproval) {
-            writeApproval();
-          } else {
-            useFeature?.();
-          }
-        } else {
-          sendChatMessage(
-            messageText.replace(/^\s*\n|\n\s*$/g, ""),
-            false,
-            isVipChat ? SenderStatus.VIP : SenderStatus.USER,
-            `${InteractionType.BLAST}:`
-          );
-          setBlastMode(false);
-          setMessageText("");
-        }
+        sendChatMessage(
+          messageText.replace(/^\s*\n|\n\s*$/g, ""),
+          false,
+          isVipChat ? SenderStatus.VIP : SenderStatus.USER,
+          `${InteractionType.BLAST}:`
+        );
+        setBlastMode(false);
       }
+      setMessageText("");
     },
-    [blastMode, messageText, requiresApproval, writeApproval, useFeature]
+    [blastMode, messageText]
   );
-
-  useEffect(() => {
-    if (!matchingChain) {
-      setTooltipError("wrong network");
-    } else if (
-      channelQueryData?.token?.address &&
-      blastMode &&
-      (!userTokenBalance?.value ||
-        (userTokenBalance?.value &&
-          parseUnits(PRICE, 18) > userTokenBalance?.value))
-    ) {
-      setTooltipError(
-        `you don't have enough ${channelQueryData?.token?.symbol} to spend`
-      );
-    } else {
-      setTooltipError("");
-    }
-  }, [channelQueryData, userTokenBalance?.value, blastMode, matchingChain]);
-
-  useEffect(() => {
-    if (
-      isApprovalSuccess &&
-      useFeature &&
-      !transitioningTxFromApprovalToExec.current
-    ) {
-      transitioningTxFromApprovalToExec.current = true;
-      useFeature();
-    }
-  }, [isApprovalSuccess, useFeature]);
 
   const toastSignIn = () => {
     toast({
@@ -502,31 +248,6 @@ const ChatForm = ({
                   close
                 </Button>
               </Flex>
-            </Flex>
-          ) : txLoading ? (
-            <Flex direction="column" gap="20px">
-              <Flex justifyContent={"center"} p="10px">
-                {gifInTransaction !== "" ? (
-                  <Image
-                    src={gifInTransaction}
-                    height="80px"
-                    className="zooming-text"
-                  />
-                ) : (
-                  <Spinner size="xl" />
-                )}
-              </Flex>
-              <Text textAlign={"center"}>
-                {isApprovalLoading
-                  ? "Approving transaction, please do not exit this page..."
-                  : "Executing transaction, please do not exit this page..."}
-              </Text>
-            </Flex>
-          ) : txTransition ? (
-            <Flex>
-              <Text textAlign={"center"} color="#ffe120">
-                Please complete the transaction in your wallet.
-              </Text>
             </Flex>
           ) : (
             <Flex direction="column" width="100%">
@@ -689,6 +410,7 @@ const ChatForm = ({
                 />
               </Flex>
               <Flex
+                mt="5px"
                 width="100%"
                 position="relative"
                 direction="column"
@@ -701,30 +423,6 @@ const ChatForm = ({
                 py="5px"
                 background={blastMode ? "rgba(255, 108, 108, 0.35)" : undefined}
               >
-                {blastMode && tooltipError === "" && (
-                  <Text
-                    color={"#b82929"}
-                    fontSize="12px"
-                    position="absolute"
-                    top={-5}
-                    whiteSpace="nowrap"
-                  >
-                    chat blast mode enabled{" "}
-                    {channelQueryData?.token?.symbol &&
-                      `(cost: ${PRICE} $${channelQueryData?.token?.symbol})`}
-                  </Text>
-                )}
-                {blastMode && tooltipError !== "" && (
-                  <Text
-                    color={"#b82929"}
-                    fontSize="12px"
-                    position="absolute"
-                    top={-5}
-                    whiteSpace="nowrap"
-                  >
-                    {tooltipError}
-                  </Text>
-                )}
                 <Flex alignItems="center">
                   <Textarea
                     size="md"
@@ -754,9 +452,7 @@ const ChatForm = ({
                   />
                   <IconButton
                     type="submit"
-                    disabled={
-                      messageTextIsEmpty || (tooltipError !== "" && blastMode)
-                    }
+                    disabled={messageTextIsEmpty}
                     icon={
                       blastMode ? (
                         <Image src="/svg/blast-send.svg" />
