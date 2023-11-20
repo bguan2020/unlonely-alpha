56c9d32067a827559c5da4f302e189c71d5a7e70
Danny Fung <dannyfung97@gmail.com>
2023-11-20 10:03:48 -0800

    more changes to the memo component

--- unlonely-alpha/web/src/components/stream/LivepeerPlayer.tsx
+++ unlonely-alpha/web/src/components/stream/LivepeerPlayer.tsx
@@ -2,42 +2,47 @@ import { Flex } from "@chakra-ui/react";
 import { Player } from "@livepeer/react";
 import { memo } from "react";
 
-const LivepeerPlayer = memo(({ playbackId }: { playbackId: string }) => {
-  const showOverlay = () => {
-    const overlay = document.getElementById("video-overlay");
+const LivepeerPlayer = memo(
+  ({ playbackId }: { playbackId: string }) => {
+    const showOverlay = () => {
+      const overlay = document.getElementById("video-overlay");
 
-    if (overlay) {
-      overlay.style.animationName = "none";
+      if (overlay) {
+        overlay.style.animationName = "none";
 
-      requestAnimationFrame(() => {
-        setTimeout(() => {
-          overlay.style.animationName = "";
-        }, 0);
-      });
-    }
-  };
+        requestAnimationFrame(() => {
+          setTimeout(() => {
+            overlay.style.animationName = "";
+          }, 0);
+        });
+      }
+    };
 
-  return (
-    <Flex
-      direction="column"
-      width={"100%"}
-      position="relative"
-      onMouseMove={showOverlay}
-      onTouchStart={showOverlay}
-    >
-      <Player
-        playbackId={playbackId}
-        aspectRatio="16to9"
-        controls={{
-          autohide: 3000,
-        }}
-        theme={{
-          borderStyles: { containerBorderStyle: undefined },
-          radii: { containerBorderRadius: "10px" },
-        }}
-      />
-    </Flex>
-  );
-});
+    return (
+      <Flex
+        direction="column"
+        width={"100%"}
+        position="relative"
+        onMouseMove={showOverlay}
+        onTouchStart={showOverlay}
+      >
+        <Player
+          playbackId={playbackId}
+          aspectRatio="16to9"
+          controls={{
+            autohide: 3000,
+          }}
+          theme={{
+            borderStyles: { containerBorderStyle: undefined },
+            radii: { containerBorderRadius: "10px" },
+          }}
+        />
+      </Flex>
+    );
+  },
+  (prevProps, nextProps) => {
+    return prevProps.playbackId === nextProps.playbackId;
+  }
+);
 
 export default LivepeerPlayer;
