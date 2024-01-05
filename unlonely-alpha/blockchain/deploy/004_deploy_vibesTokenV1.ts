5940e80000b8af15f2eb8316d6a363d2fc94f73e
Danny Fung <dannyfung97@gmail.com>
2024-01-05 11:10:22 -0800

    created deploy script for VibesToken

---
+++ unlonely-alpha/blockchain/deploy/004_deploy_vibesTokenV1.ts
@@ -0,0 +1,19 @@
+import {HardhatRuntimeEnvironment} from "hardhat/types";
+import {DeployFunction} from "hardhat-deploy/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deploy } = hre.deployments;
+  const { deployer } = await hre.getNamedAccounts();
+
+  await deploy("VibesTokenV1", {
+    from: deployer,
+    args: [],
+    log: true
+  })
+};
+export default func;
+
+func.tags = ["All", "VibesTokenV1"]
+
+// npx hardhat deploy --network base_goerli --tags All,VibesTokenV1
+// npx hardhat --network base_goerli etherscan-verify
