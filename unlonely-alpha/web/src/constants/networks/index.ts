5795dde9118a1eb2c5c68c0f95de92451ecacaf5
Danny Fung <dannyfung97@gmail.com>
2024-01-19 16:59:55 -0800

    removed todo comment

--- unlonely-alpha/web/src/constants/networks/index.ts
+++ unlonely-alpha/web/src/constants/networks/index.ts
@@ -8,6 +8,4 @@ export * from "./goerli";
 export * from "./baseGoerli";
 export * from "./base";
 
-// eslint-disable-next-line no-warning-comments
-// TODO: base goerli back to last later
 export const NETWORKS = [Base, Mainnet, Goerli, BaseGoerli];
