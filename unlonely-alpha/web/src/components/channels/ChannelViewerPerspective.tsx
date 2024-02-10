06680810c2cc66f32b419ab316910e27da2b7f87
Danny Fung <dannyfung97@gmail.com>
2024-02-09 18:29:48 -0800

    trying to find the height of keyboard when typing

--- unlonely-alpha/web/src/components/channels/ChannelViewerPerspective.tsx
+++ unlonely-alpha/web/src/components/channels/ChannelViewerPerspective.tsx
@@ -2,21 +2,34 @@ import { Flex, Stack } from "@chakra-ui/react";
 
 import StreamComponent from "../stream/StreamComponent";
 import { useCacheContext } from "../../hooks/context/useCache";
+import { useEffect, useState } from "react";
+import { Text } from "@chakra-ui/react";
 
 const ChannelViewerPerspective = ({ mobile }: { mobile?: boolean }) => {
   const { isFocusedOnInput } = useCacheContext();
+
+  const initialViewportHeight = window.innerHeight;
+  const [num, setNum] = useState(initialViewportHeight);
+
+  useEffect(() => {
+    if (isFocusedOnInput) {
+      setNum(initialViewportHeight - window.innerHeight);
+    } else {
+      setNum(initialViewportHeight);
+    }
+  }, [isFocusedOnInput]);
 
   return (
     <Stack
       direction="column"
       width={"100%"}
       position={mobile ? "fixed" : "unset"}
-      bottom={isFocusedOnInput ? "160px" : "unset"}
       zIndex={5}
     >
       <Flex width={"100%"} position="relative">
         <StreamComponent />
       </Flex>
+      <Text>temp: {num}</Text>
     </Stack>
   );
 };
