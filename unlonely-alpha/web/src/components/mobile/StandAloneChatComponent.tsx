8373ae3cc13fdf423ba2ec515bceb180f4cd059e
Danny Fung <dannyfung97@gmail.com>
2024-02-10 12:24:34 -0800

    return to no margin top

--- unlonely-alpha/web/src/components/mobile/StandAloneChatComponent.tsx
+++ unlonely-alpha/web/src/components/mobile/StandAloneChatComponent.tsx
@@ -27,7 +27,11 @@ import { useChannelContext } from "../../hooks/context/useChannel";
 import { useUser } from "../../hooks/context/useUser";
 import ChannelDesc from "../channels/ChannelDesc";
 import { ChatReturnType, useChatBox } from "../../hooks/chat/useChat";
-import { ADD_REACTION_EVENT, MOBILE_VIDEO_VH } from "../../constants";
+import {
+  ADD_REACTION_EVENT,
+  MOBILE_CHAT_VH,
+  MOBILE_VIDEO_VH,
+} from "../../constants";
 import MessageList from "../chat/MessageList";
 import ChatForm from "../chat/ChatForm";
 import { GET_SUBSCRIPTION } from "../../constants/queries";
@@ -232,10 +236,9 @@ const StandaloneChatComponent = ({
         // mobileSizes.keyboardVisible || isFocusedOnInput
         //   ? `calc(${mobileSizes.viewport.height}px - ${MOBILE_VIDEO_VH}vh)`
         //   :
-        // !previewStream && isOwner
-        //   ? `${MOBILE_CHAT_VH + MOBILE_VIDEO_VH}vh`
-        //   : `${MOBILE_CHAT_VH}vh`
-        `calc(${mobileSizes.viewport.height}px - ${MOBILE_VIDEO_VH}vh)`
+        !previewStream && isOwner
+          ? `${MOBILE_CHAT_VH + MOBILE_VIDEO_VH}vh`
+          : `${MOBILE_CHAT_VH}vh`
       }
       p="5px"
       id="chat"
@@ -244,8 +247,7 @@ const StandaloneChatComponent = ({
         // mobileSizes.keyboardVisible || isFocusedOnInput
         //   ? `calc(${newTop}px + ${MOBILE_VIDEO_VH}vh)`
         //   :
-        // !previewStream && isOwner ? "0" : `${MOBILE_VIDEO_VH}vh`
-        `calc(${newTop}px + ${MOBILE_VIDEO_VH}vh)`
+        !previewStream && isOwner ? "0" : `${MOBILE_VIDEO_VH}vh`
       }
     >
       {chatChannel?.includes("channel") ? (
