c5e28cb992d049f048ff3b602532b7f302253111
Danny Fung <dannyfung97@gmail.com>
2024-02-13 13:19:28 -0800

    fixed full vibes chart

--- unlonely-alpha/web/src/pages/vibes/[slug].tsx
+++ unlonely-alpha/web/src/pages/vibes/[slug].tsx
@@ -1,9 +1,17 @@
 import { Flex } from "@chakra-ui/react";
+import { useQuery } from "@apollo/client";
 
 import VibesTokenInterface from "../../components/chat/VibesTokenInterface";
-import { ChannelProvider } from "../../hooks/context/useChannel";
+import {
+  ChannelProvider,
+  useChannelContext,
+} from "../../hooks/context/useChannel";
 import { useChat } from "../../hooks/chat/useChat";
 import AppLayout from "../../components/layout/AppLayout";
+import { ChannelStaticQuery } from "../../generated/graphql";
+import { useRouter } from "next/router";
+import { CHANNEL_STATIC_QUERY } from "../../constants/queries";
+import { useEffect } from "react";
 
 const FullVibesChartPage = () => {
   return (
@@ -18,9 +26,34 @@ const FullVibesChartPage = () => {
 const FullVibesChart = () => {
   const chat = useChat();
 
+  const router = useRouter();
+  const { slug } = router.query;
+  const { channel } = useChannelContext();
+  const { handleChannelStaticData } = channel;
+  const {
+    data: channelStatic,
+    error: channelStaticError,
+    loading: channelStaticLoading,
+  } = useQuery<ChannelStaticQuery>(CHANNEL_STATIC_QUERY, {
+    variables: { slug },
+    fetchPolicy: "cache-and-network",
+  });
+
+  useEffect(() => {
+    if (channelStatic)
+      handleChannelStaticData(
+        (channelStatic["getChannelBySlug"] as any) ?? null
+      );
+  }, [channelStatic]);
+
   return (
     <Flex h="100vh" justifyContent={"space-between"} bg="#131323" p="0.5rem">
-      <VibesTokenInterface isFullChart ablyChannel={chat.channel} />
+      <VibesTokenInterface
+        isFullChart
+        ablyChannel={chat.channel}
+        customLoading={channelStaticLoading}
+        noChannelData={channelStaticError !== undefined}
+      />
     </Flex>
   );
 };
