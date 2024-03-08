9a804b2c70aa19b649ee3fc6410b8332cec15eaa
Danny Fung <dannyfung97@gmail.com>
2024-03-08 15:06:07 -0800

    added any to watchblocks

--- unlonely-alpha/server/src/utils/watchBlock.ts
+++ unlonely-alpha/server/src/utils/watchBlock.ts
@@ -6,12 +6,12 @@ const vibesTokenContractAddress = "0xEAB1fF15f26da850315b15AFebf12F0d42dE5421"
 
 export const watchBlocks = () => {
     const publicClient = createPublicClient({
-        chain: base,
+        chain: base as any,
         transport: viemHttp(
           `https://base-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_BASE_API_KEY}`
         ),
       });
-      publicClient.watchBlocks( 
+      (publicClient as any).watchBlocks( 
         { onBlock: async() => {
           await postVibesTrades({ chainId: 8453, tokenAddress: vibesTokenContractAddress})
         } }
