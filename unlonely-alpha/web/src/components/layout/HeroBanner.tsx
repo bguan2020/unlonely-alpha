5c3398e5c0d10d8a6b8c4a43602ac5d37cadb3b2
Danny Fung <dannyfung97@gmail.com>
2024-03-11 19:53:32 -0700

    create channel banner ui improvement

--- unlonely-alpha/web/src/components/layout/HeroBanner.tsx
+++ unlonely-alpha/web/src/components/layout/HeroBanner.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Text } from "@chakra-ui/react";
+import { Button, Flex, Text, Image } from "@chakra-ui/react";
 
 const HeroBanner = () => {
   const redirectToNewChannelPage = () => {
@@ -9,7 +9,7 @@ const HeroBanner = () => {
     <Flex
       mt="20px"
       gap="10px"
-      h="30vh"
+      h="35vh"
       direction={["column", "column", "row"]}
       alignItems={"center"}
       justifyContent={"space-evenly"}
@@ -17,27 +17,51 @@ const HeroBanner = () => {
       p="10px"
     >
       <Flex direction="column">
-        <Text fontSize={["1rem", "3rem"]} fontWeight="bold">
+        <Text
+          fontSize={["2rem", "3rem", "3rem", "4rem"]}
+          fontWeight="600"
+          fontFamily="Inter"
+        >
           Make a Channel.
         </Text>
-        <Text fontSize={["1rem", "3rem"]} fontWeight="bold">
+        <Text
+          fontSize={["2rem", "3rem", "3rem", "4rem"]}
+          fontWeight="600"
+          fontFamily="Inter"
+        >
           Start Streaming.
         </Text>
       </Flex>
       <Flex direction="column">
-        <Button
-          _hover={{
-            transform: "scale(1.1)",
-          }}
-          _active={{}}
-          _focus={{}}
-          onClick={redirectToNewChannelPage}
-          p={[4, 6, 8, 10]}
-        >
-          <Text fontSize={["1rem", "2rem"]} fontWeight="bold">
-            Create Channel
-          </Text>
-        </Button>
+        <Flex justifyContent={"center"}>
+          <Button
+            border="2px solid white"
+            borderRadius="20px"
+            color="white"
+            bg="rgba(70, 168, 0, 1)"
+            onClick={redirectToNewChannelPage}
+            _hover={{
+              bg: "rgba(70, 168, 0, 0.8)",
+            }}
+            position={"relative"}
+            _active={{}}
+            _focus={{}}
+            p={[6, 8, 10, 12]}
+          >
+            <Image
+              src="/images/sparkles.png"
+              position="absolute"
+              zIndex="2"
+              minWidth={["140%", "140%", "140%", "140%"]}
+            />
+            <Text
+              fontSize={["1rem", "2rem", "2srem", "3rem"]}
+              fontFamily="LoRes15"
+            >
+              create channel
+            </Text>
+          </Button>
+        </Flex>
       </Flex>
     </Flex>
   );
