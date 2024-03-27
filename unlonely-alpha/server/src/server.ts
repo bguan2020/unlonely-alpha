51ae034135537b29ed63e8698f5ed23ac71903dc
Danny Fung <dannyfung97@gmail.com>
2024-03-27 13:47:18 -0700

    added comments in aws-scheduler-update

--- unlonely-alpha/server/src/server.ts
+++ unlonely-alpha/server/src/server.ts
@@ -31,10 +31,10 @@ app.get("/aws-scheduler-update", (req, res) => {
   const secretKey = req.headers["x-secret-key"] || req.query.secretKey;
 
   if (secretKey !== process.env.AWS_ACCESS_KEY) {
-    console.log("Unauthorized access to /aws-scheduler-update")
+    console.log("Unauthorized access to /aws-scheduler-update, called at", Date.now())
     return res.status(401).send("Unauthorized");
   }
-  console.log("Authorized access to /aws-scheduler-update")
+  console.log("Authorized access to /aws-scheduler-update, called at", Date.now())
   res.send("/aws-scheduler-update success");
 });
 
