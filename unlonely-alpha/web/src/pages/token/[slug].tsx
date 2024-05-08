b386a87a959d0782f55ca6624e76906498121dac
Danny Fung <dannyfung97@gmail.com>
2024-05-08 15:47:06 -0700

    added ably message intercepters in pages/token/[slug] to match desktop counterparts

--- unlonely-alpha/web/src/pages/token/[slug].tsx
+++ unlonely-alpha/web/src/pages/token/[slug].tsx
@@ -1,7 +1,7 @@
 import { Flex } from "@chakra-ui/react";
 import { useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import {
   ChannelProvider,
@@ -12,10 +12,26 @@ import AppLayout from "../../components/layout/AppLayout";
 import { ChannelStaticQuery } from "../../generated/graphql";
 import { CHANNEL_STATIC_QUERY } from "../../constants/queries";
 import { TempTokenInterface } from "../../components/channels/temp/TempTokenInterface";
-import { TempTokenProvider } from "../../hooks/context/useTempToken";
-import { VersusTempTokenProvider } from "../../hooks/context/useVersusTempToken";
+import {
+  TempTokenProvider,
+  useTempTokenContext,
+} from "../../hooks/context/useTempToken";
+import {
+  VersusTempTokenProvider,
+  useVersusTempTokenContext,
+} from "../../hooks/context/useVersusTempToken";
 import { VersusTempTokensInterface } from "../../components/channels/layout/versus/VersusTempTokensInterface";
-import { CAN_USE_VERSUS_MODE_SLUGS } from "../../constants";
+import {
+  CAN_USE_VERSUS_MODE_SLUGS,
+  CHAT_MESSAGE_EVENT,
+  InteractionType,
+  versusTokenDataInitial,
+} from "../../constants";
+import { isAddress, isAddressEqual } from "viem";
+import useDebounce from "../../hooks/internal/useDebounce";
+
+import TempTokenAbi from "../../constants/abi/TempTokenV1.json";
+import { useUser } from "../../hooks/context/useUser";
 
 const FullTempTokenChartPage = () => {
   return (
@@ -76,7 +92,171 @@ const FullTempTokenChart = ({
   channelStaticError?: any;
   channelStaticLoading?: boolean;
 }) => {
+  const { userAddress } = useUser();
+  const { channel } = useChannelContext();
+  const { tempToken } = useTempTokenContext();
   const chat = useChat();
+  const { handleRealTimeChannelDetails } = channel;
+  const {
+    currentTempTokenContract,
+    currentActiveTokenAddress,
+    lastInactiveTokenAddress,
+    tempTokenTxs,
+    handleIsGamePermanent,
+    handleIsGameSuccess,
+    handleIsGameFailed,
+    resetTempTokenTxs,
+    handleCurrentActiveTokenEndTimestamp,
+    handleCurrentActiveTokenCreationBlockNumber,
+    handleCurrentActiveTokenAddress,
+    handleCurrentActiveTokenSymbol,
+    handleCurrentActiveTokenTotalSupplyThreshold,
+    onMintEvent,
+    onBurnEvent,
+    onReachThresholdEvent,
+    getTempTokenEvents,
+    refetchUserTempTokenBalance,
+    onSendRemainingFundsToWinnerEvent,
+  } = tempToken;
+  const mountingMessages = useRef(true);
+  const fetching = useRef(false);
+  const [blockNumberOfLastInAppTrade, setBlockNumberOfLastInAppTrade] =
+    useState<bigint>(BigInt(0));
+
+  useEffect(() => {
+    if (chat.mounted) mountingMessages.current = false;
+  }, [chat.mounted]);
+
+  const [tempTokenTransactionBody, setTempTokenTransactionBody] = useState("");
+  const debouncedTempTokenTransactionBody = useDebounce(
+    tempTokenTransactionBody,
+    500
+  );
+
+  useEffect(() => {
+    const processTempTokenEvents = async (body: string) => {
+      console.log("fetching temp token events 1", body);
+      if (!body || fetching.current) return;
+      fetching.current = true;
+      const interactionType = body.split(":")[0];
+      const _userAddress = body.split(":")[1];
+      const txBlockNumber = BigInt(body.split(":")[3]);
+      const incomingTxTokenAddress = body.split(":")[4];
+      const totalSupply = BigInt(body.split(":")[5]);
+      console.log("fetching temp token events 2");
+      if (
+        currentTempTokenContract.address &&
+        isAddress(incomingTxTokenAddress) &&
+        isAddress(currentTempTokenContract.address) &&
+        isAddressEqual(
+          incomingTxTokenAddress as `0x${string}`,
+          currentTempTokenContract.address as `0x${string}`
+        )
+      ) {
+        console.log("fetching temp token events 3");
+        await getTempTokenEvents(
+          currentTempTokenContract,
+          blockNumberOfLastInAppTrade === BigInt(0) && tempTokenTxs.length > 0
+            ? BigInt(tempTokenTxs[tempTokenTxs.length - 1].blockNumber)
+            : blockNumberOfLastInAppTrade,
+          txBlockNumber
+        );
+        if (
+          userAddress &&
+          isAddress(userAddress) &&
+          isAddress(_userAddress) &&
+          isAddressEqual(
+            userAddress as `0x${string}`,
+            _userAddress as `0x${string}`
+          )
+        ) {
+          refetchUserTempTokenBalance?.();
+        }
+        setBlockNumberOfLastInAppTrade(txBlockNumber);
+        if (interactionType === InteractionType.BUY_TEMP_TOKENS) {
+          const highestTotalSupply = body.split(":")[6];
+          const hasHitTotalSupplyThreshold = body.split(":")[7] === "true";
+          const newEndTimestampForToken = BigInt(body.split(":")[8]);
+          if (hasHitTotalSupplyThreshold) {
+            onReachThresholdEvent(newEndTimestampForToken);
+          }
+          onMintEvent(BigInt(totalSupply), BigInt(highestTotalSupply));
+        }
+        if (interactionType === InteractionType.SELL_TEMP_TOKENS)
+          onBurnEvent(BigInt(totalSupply));
+      }
+      fetching.current = false;
+    };
+    processTempTokenEvents(debouncedTempTokenTransactionBody);
+  }, [debouncedTempTokenTransactionBody]);
+
+  useEffect(() => {
+    if (chat.receivedMessages.length === 0) return;
+    const latestMessage =
+      chat.receivedMessages[chat.receivedMessages.length - 1];
+    if (
+      latestMessage &&
+      latestMessage.data.body &&
+      latestMessage.name === CHAT_MESSAGE_EVENT &&
+      Date.now() - latestMessage.timestamp < 12000
+    ) {
+      const body = latestMessage.data.body;
+      if (
+        body.split(":")[0] === InteractionType.CREATE_TEMP_TOKEN &&
+        Date.now() - latestMessage.timestamp < 12000
+      ) {
+        handleRealTimeChannelDetails({
+          isLive: true,
+        });
+
+        handleIsGamePermanent(false);
+        handleIsGameSuccess(false);
+        handleIsGameFailed(false);
+        resetTempTokenTxs();
+
+        handleCurrentActiveTokenEndTimestamp(BigInt(body.split(":")[7]));
+        handleCurrentActiveTokenCreationBlockNumber(BigInt(body.split(":")[8]));
+        handleCurrentActiveTokenAddress(body.split(":")[1]);
+        handleCurrentActiveTokenSymbol(body.split(":")[2]);
+        handleCurrentActiveTokenTotalSupplyThreshold(
+          BigInt(body.split(":")[9])
+        );
+      }
+      if (
+        body.split(":")[0] === InteractionType.BUY_TEMP_TOKENS ||
+        body.split(":")[0] === InteractionType.SELL_TEMP_TOKENS
+      ) {
+        setTempTokenTransactionBody(body);
+      }
+      if (
+        body.split(":")[0] ===
+        InteractionType.SEND_REMAINING_FUNDS_TO_WINNER_AFTER_TEMP_TOKEN_EXPIRATION
+      ) {
+        const to = body.split(":")[3];
+
+        if (
+          isAddress(to) &&
+          isAddress(lastInactiveTokenAddress) &&
+          isAddressEqual(to, lastInactiveTokenAddress)
+        ) {
+          onSendRemainingFundsToWinnerEvent(
+            lastInactiveTokenAddress as `0x${string}`,
+            false
+          );
+        }
+        if (
+          isAddress(to) &&
+          isAddress(currentActiveTokenAddress) &&
+          isAddressEqual(to, currentActiveTokenAddress)
+        ) {
+          onSendRemainingFundsToWinnerEvent(
+            currentActiveTokenAddress as `0x${string}`,
+            true
+          );
+        }
+      }
+    }
+  }, [chat.receivedMessages]);
 
   return (
     <Flex h="100vh" justifyContent={"space-between"} bg="#131323" p="0.5rem">
@@ -97,7 +277,193 @@ const FullVersusTokenChart = ({
   channelStaticError?: any;
   channelStaticLoading?: boolean;
 }) => {
+  const { userAddress } = useUser();
+  const { channel } = useChannelContext();
   const chat = useChat();
+  const { handleRealTimeChannelDetails } = channel;
+  const mountingMessages = useRef(true);
+  const fetching = useRef(false);
+
+  const { gameState, tokenATxs, tokenBTxs, callbacks } =
+    useVersusTempTokenContext();
+  const {
+    handleIsGameFinished,
+    handleIsGameOngoing,
+    handleOwnerMustPermamint,
+    handleOwnerMustMakeWinningTokenTradeable,
+    handleIsGameFinishedModalOpen,
+    handleWinningToken,
+    handleLosingToken,
+    setTokenA,
+    setTokenB,
+    tokenA,
+    tokenB,
+  } = gameState;
+  const {
+    resetTempTokenTxs: resetTempTokenTxsA,
+    getTempTokenEvents: getTempTokenEventsA,
+    refetchUserTempTokenBalance: refetchUserTempTokenBalanceA,
+    tempTokenTxs: tempTokenTxsA,
+  } = tokenATxs;
+  const {
+    resetTempTokenTxs: resetTempTokenTxsB,
+    getTempTokenEvents: getTempTokenEventsB,
+    refetchUserTempTokenBalance: refetchUserTempTokenBalanceB,
+    tempTokenTxs: tempTokenTxsB,
+  } = tokenBTxs;
+  const { onMintEvent, onBurnEvent } = callbacks;
+
+  const [blockNumberOfLastInAppTrade, setBlockNumberOfLastInAppTrade] =
+    useState<bigint>(BigInt(0));
+
+  useEffect(() => {
+    if (chat.mounted) mountingMessages.current = false;
+  }, [chat.mounted]);
+
+  const [tempTokenTransactionBody, setTempTokenTransactionBody] = useState("");
+  const debouncedTempTokenTransactionBody = useDebounce(
+    tempTokenTransactionBody,
+    500
+  );
+
+  useEffect(() => {
+    const processVersusTokenEvents = async (body: string) => {
+      console.log("fetching versus token events 1");
+      if (!body || fetching.current) return;
+      fetching.current = true;
+      const interactionType = body.split(":")[0];
+      const _userAddress = body.split(":")[1];
+      const txBlockNumber = BigInt(body.split(":")[3]);
+      const incomingTxTokenAddress = body.split(":")[4];
+      const totalSupply = BigInt(body.split(":")[5]);
+      const highestTotalSupply = body.split(":")[6];
+      console.log("fetching versus token events 2");
+      const tokenType =
+        isAddress(tokenB.address) &&
+        isAddress(incomingTxTokenAddress) &&
+        isAddressEqual(tokenB.address, incomingTxTokenAddress)
+          ? "b"
+          : "a";
+      await Promise.all([
+        getTempTokenEventsA(
+          tokenA.contractData,
+          blockNumberOfLastInAppTrade === BigInt(0) && tempTokenTxsA.length > 0
+            ? BigInt(tempTokenTxsA[tempTokenTxsA.length - 1].blockNumber)
+            : blockNumberOfLastInAppTrade,
+          txBlockNumber
+        ),
+        getTempTokenEventsB(
+          tokenB.contractData,
+          blockNumberOfLastInAppTrade === BigInt(0) && tempTokenTxsB.length > 0
+            ? BigInt(tempTokenTxsB[tempTokenTxsB.length - 1].blockNumber)
+            : blockNumberOfLastInAppTrade,
+          txBlockNumber
+        ),
+      ]);
+      if (
+        userAddress &&
+        isAddress(userAddress) &&
+        isAddress(_userAddress) &&
+        isAddressEqual(
+          userAddress as `0x${string}`,
+          _userAddress as `0x${string}`
+        )
+      ) {
+        if (tokenType === "a") {
+          refetchUserTempTokenBalanceA?.();
+        } else {
+          refetchUserTempTokenBalanceB?.();
+        }
+      }
+      setBlockNumberOfLastInAppTrade(txBlockNumber);
+      if (interactionType === InteractionType.BUY_TEMP_TOKENS)
+        onMintEvent(BigInt(totalSupply), BigInt(highestTotalSupply), tokenType);
+      if (interactionType === InteractionType.SELL_TEMP_TOKENS)
+        onBurnEvent(BigInt(totalSupply), tokenType);
+      fetching.current = false;
+    };
+    processVersusTokenEvents(debouncedTempTokenTransactionBody);
+  }, [debouncedTempTokenTransactionBody]);
+
+  useEffect(() => {
+    const checkAbly = async () => {
+      if (chat.receivedMessages.length === 0) return;
+      const latestMessage =
+        chat.receivedMessages[chat.receivedMessages.length - 1];
+      if (
+        latestMessage &&
+        latestMessage.data.body &&
+        latestMessage.name === CHAT_MESSAGE_EVENT &&
+        Date.now() - latestMessage.timestamp < 12000
+      ) {
+        const body = latestMessage.data.body;
+        console.log(
+          "DesktopChannelPageVersus -> body",
+          body,
+          blockNumberOfLastInAppTrade
+        );
+        if (
+          body.split(":")[0] === InteractionType.CREATE_MULTIPLE_TEMP_TOKENS
+        ) {
+          const newEndTimestamp = BigInt(body.split(":")[1]);
+          const newTokenAddresses = JSON.parse(body.split(":")[2]);
+          const newTokenSymbols = JSON.parse(body.split(":")[3]);
+          const chainId = Number(body.split(":")[4]);
+          const newTokenCreationBlockNumber = BigInt(body.split(":")[5]);
+          handleRealTimeChannelDetails({
+            isLive: true,
+          });
+
+          handleIsGameFinished(false);
+          handleIsGameOngoing(true);
+          handleOwnerMustPermamint(false);
+          handleOwnerMustMakeWinningTokenTradeable(false);
+          handleIsGameFinishedModalOpen(false);
+          handleWinningToken(versusTokenDataInitial);
+          handleLosingToken(versusTokenDataInitial);
+          resetTempTokenTxsA();
+          resetTempTokenTxsB();
+          setTokenA({
+            transferredLiquidityOnExpiration: BigInt(0),
+            symbol: newTokenSymbols[0],
+            address: newTokenAddresses[0],
+            totalSupply: BigInt(0),
+            isAlwaysTradeable: false,
+            highestTotalSupply: BigInt(0),
+            contractData: {
+              address: newTokenAddresses[0],
+              chainId,
+              abi: TempTokenAbi,
+            },
+            creationBlockNumber: newTokenCreationBlockNumber,
+            endTimestamp: newEndTimestamp,
+          });
+          setTokenB({
+            transferredLiquidityOnExpiration: BigInt(0),
+            symbol: newTokenSymbols[1],
+            address: newTokenAddresses[1],
+            totalSupply: BigInt(0),
+            isAlwaysTradeable: false,
+            highestTotalSupply: BigInt(0),
+            contractData: {
+              address: newTokenAddresses[1],
+              chainId,
+              abi: TempTokenAbi,
+            },
+            creationBlockNumber: newTokenCreationBlockNumber,
+            endTimestamp: newEndTimestamp,
+          });
+        }
+        if (
+          body.split(":")[0] === InteractionType.BUY_TEMP_TOKENS ||
+          body.split(":")[0] === InteractionType.SELL_TEMP_TOKENS
+        ) {
+          setTempTokenTransactionBody(body);
+        }
+      }
+    };
+    checkAbly();
+  }, [chat.receivedMessages]);
 
   return (
     <Flex h="100vh" justifyContent={"space-between"} bg="#131323" p="0.5rem">
