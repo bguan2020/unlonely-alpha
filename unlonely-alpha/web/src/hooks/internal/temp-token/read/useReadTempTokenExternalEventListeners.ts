fc6ce15dfc5a88b289c46768e811b016a819badb
Danny Fung <dannyfung97@gmail.com>
2024-05-09 09:30:30 -0700

    try commenting out admin triggered listeners

--- unlonely-alpha/web/src/hooks/internal/temp-token/read/useReadTempTokenExternalEventListeners.ts
+++ unlonely-alpha/web/src/hooks/internal/temp-token/read/useReadTempTokenExternalEventListeners.ts
@@ -1,6 +1,3 @@
-import { useState, useEffect } from "react";
-import { Log, isAddressEqual } from "viem";
-import { useContractEvent } from "wagmi";
 import { ContractData } from "../../../../constants/types";
 
 export const useReadTempTokenExternalEventListeners = ({
@@ -22,141 +19,141 @@ export const useReadTempTokenExternalEventListeners = ({
   ) => void;
 }) => {
 
-  /**
-   * listen for duration increase event
-   */
+  // /**
+  //  * listen for duration increase event
+  //  */
 
-  const [
-    incomingTempTokenDurationExtendedLogs,
-    setIncomingTempTokenDurationExtendedLogs,
-  ] = useState<Log[]>([]);
+  // const [
+  //   incomingTempTokenDurationExtendedLogs,
+  //   setIncomingTempTokenDurationExtendedLogs,
+  // ] = useState<Log[]>([]);
 
-  useContractEvent({
-    address: tempTokenContract.address,
-    abi: tempTokenContract.abi,
-    eventName: "TokenDurationExtended",
-    listener(logs) {
-      console.log("detected TokenDurationExtended event", logs);
-      const init = async () => {
-        setIncomingTempTokenDurationExtendedLogs(logs);
-      };
-      init();
-    },
-  });
+  // useContractEvent({
+  //   address: tempTokenContract.address,
+  //   abi: tempTokenContract.abi,
+  //   eventName: "TokenDurationExtended",
+  //   listener(logs) {
+  //     console.log("detected TokenDurationExtended event", logs);
+  //     const init = async () => {
+  //       setIncomingTempTokenDurationExtendedLogs(logs);
+  //     };
+  //     init();
+  //   },
+  // });
 
-  useEffect(() => {
-    if (incomingTempTokenDurationExtendedLogs)
-      handleTempTokenDurationExtendedUpdate(
-        incomingTempTokenDurationExtendedLogs
-      );
-  }, [incomingTempTokenDurationExtendedLogs]);
+  // useEffect(() => {
+  //   if (incomingTempTokenDurationExtendedLogs)
+  //     handleTempTokenDurationExtendedUpdate(
+  //       incomingTempTokenDurationExtendedLogs
+  //     );
+  // }, [incomingTempTokenDurationExtendedLogs]);
 
-  const handleTempTokenDurationExtendedUpdate = async (logs: Log[]) => {
-    if (logs.length === 0) return;
-    const filteredLogsByTokenAddress = logs.filter((log: any) =>
-      isAddressEqual(
-        log.address as `0x${string}`,
-        tempTokenContract.address as `0x${string}`
-      )
-    );
-    const sortedLogs = filteredLogsByTokenAddress.sort(
-      (a, b) => Number(a.blockNumber) - Number(b.blockNumber)
-    );
-    if (sortedLogs.length === 0) return;
-    const latestLog: any = sortedLogs[sortedLogs.length - 1];
-    const newEndTimestamp = latestLog?.args.endTimestamp as bigint;
-    onDurationIncreaseCallback(newEndTimestamp);
-  };
+  // const handleTempTokenDurationExtendedUpdate = async (logs: Log[]) => {
+  //   if (logs.length === 0) return;
+  //   const filteredLogsByTokenAddress = logs.filter((log: any) =>
+  //     isAddressEqual(
+  //       log.address as `0x${string}`,
+  //       tempTokenContract.address as `0x${string}`
+  //     )
+  //   );
+  //   const sortedLogs = filteredLogsByTokenAddress.sort(
+  //     (a, b) => Number(a.blockNumber) - Number(b.blockNumber)
+  //   );
+  //   if (sortedLogs.length === 0) return;
+  //   const latestLog: any = sortedLogs[sortedLogs.length - 1];
+  //   const newEndTimestamp = latestLog?.args.endTimestamp as bigint;
+  //   onDurationIncreaseCallback(newEndTimestamp);
+  // };
 
-  /**
-   * listen for always tradeable event
-   */
+  // /**
+  //  * listen for always tradeable event
+  //  */
 
-  const [
-    incomingTempTokenAlwaysTradeableSetLogs,
-    setIncomingTempTokenAlwaysTradeableSetLogs,
-  ] = useState<Log[]>([]);
+  // const [
+  //   incomingTempTokenAlwaysTradeableSetLogs,
+  //   setIncomingTempTokenAlwaysTradeableSetLogs,
+  // ] = useState<Log[]>([]);
 
-  useContractEvent({
-    address: tempTokenContract.address,
-    abi: tempTokenContract.abi,
-    eventName: "TokenAlwaysTradeableSet",
-    listener(logs) {
-      console.log("detected TokenAlwaysTradeableSet event", logs);
-      const init = async () => {
-        setIncomingTempTokenAlwaysTradeableSetLogs(logs);
-      };
-      init();
-    },
-  });
+  // useContractEvent({
+  //   address: tempTokenContract.address,
+  //   abi: tempTokenContract.abi,
+  //   eventName: "TokenAlwaysTradeableSet",
+  //   listener(logs) {
+  //     console.log("detected TokenAlwaysTradeableSet event", logs);
+  //     const init = async () => {
+  //       setIncomingTempTokenAlwaysTradeableSetLogs(logs);
+  //     };
+  //     init();
+  //   },
+  // });
 
-  useEffect(() => {
-    if (incomingTempTokenAlwaysTradeableSetLogs)
-      handleTempTokenAlwaysTradeableSetUpdate(
-        incomingTempTokenAlwaysTradeableSetLogs
-      );
-  }, [incomingTempTokenAlwaysTradeableSetLogs]);
+  // useEffect(() => {
+  //   if (incomingTempTokenAlwaysTradeableSetLogs)
+  //     handleTempTokenAlwaysTradeableSetUpdate(
+  //       incomingTempTokenAlwaysTradeableSetLogs
+  //     );
+  // }, [incomingTempTokenAlwaysTradeableSetLogs]);
 
-  const handleTempTokenAlwaysTradeableSetUpdate = async (logs: Log[]) => {
-    if (logs.length === 0) return;
-    const filteredLogsByTokenAddress = logs.filter((log: any) =>
-      isAddressEqual(
-        log.address as `0x${string}`,
-        tempTokenContract.address as `0x${string}`
-      )
-    );
-    const sortedLogs = filteredLogsByTokenAddress.sort(
-      (a, b) => Number(a.blockNumber) - Number(b.blockNumber)
-    );
-    if (sortedLogs.length === 0) return;
-    onAlwaysTradeableCallback();
-  };
+  // const handleTempTokenAlwaysTradeableSetUpdate = async (logs: Log[]) => {
+  //   if (logs.length === 0) return;
+  //   const filteredLogsByTokenAddress = logs.filter((log: any) =>
+  //     isAddressEqual(
+  //       log.address as `0x${string}`,
+  //       tempTokenContract.address as `0x${string}`
+  //     )
+  //   );
+  //   const sortedLogs = filteredLogsByTokenAddress.sort(
+  //     (a, b) => Number(a.blockNumber) - Number(b.blockNumber)
+  //   );
+  //   if (sortedLogs.length === 0) return;
+  //   onAlwaysTradeableCallback();
+  // };
 
-  /**
-   * listen for threshold update event
-   */
+  // /**
+  //  * listen for threshold update event
+  //  */
 
-  const [
-    incomingTempTokenTotalSupplyThresholdUpdatedLogs,
-    setIncomingTempTokenTotalSupplyThresholdUpdatedLogs,
-  ] = useState<Log[]>([]);
+  // const [
+  //   incomingTempTokenTotalSupplyThresholdUpdatedLogs,
+  //   setIncomingTempTokenTotalSupplyThresholdUpdatedLogs,
+  // ] = useState<Log[]>([]);
 
-  useContractEvent({
-    address: tempTokenContract.address,
-    abi: tempTokenContract.abi,
-    eventName: "TotalSupplyThresholdUpdated",
-    listener(logs) {
-      console.log("detected TotalSupplyThresholdUpdated event", logs);
-      const init = async () => {
-        setIncomingTempTokenTotalSupplyThresholdUpdatedLogs(logs);
-      };
-      init();
-    },
-  });
+  // useContractEvent({
+  //   address: tempTokenContract.address,
+  //   abi: tempTokenContract.abi,
+  //   eventName: "TotalSupplyThresholdUpdated",
+  //   listener(logs) {
+  //     console.log("detected TotalSupplyThresholdUpdated event", logs);
+  //     const init = async () => {
+  //       setIncomingTempTokenTotalSupplyThresholdUpdatedLogs(logs);
+  //     };
+  //     init();
+  //   },
+  // });
 
-  useEffect(() => {
-    if (incomingTempTokenTotalSupplyThresholdUpdatedLogs)
-      handleTempTokenTotalSupplyThresholdUpdatedUpdate(
-        incomingTempTokenTotalSupplyThresholdUpdatedLogs
-      );
-  }, [incomingTempTokenTotalSupplyThresholdUpdatedLogs]);
+  // useEffect(() => {
+  //   if (incomingTempTokenTotalSupplyThresholdUpdatedLogs)
+  //     handleTempTokenTotalSupplyThresholdUpdatedUpdate(
+  //       incomingTempTokenTotalSupplyThresholdUpdatedLogs
+  //     );
+  // }, [incomingTempTokenTotalSupplyThresholdUpdatedLogs]);
 
-  const handleTempTokenTotalSupplyThresholdUpdatedUpdate = async (
-    logs: Log[]
-  ) => {
-    if (logs.length === 0) return;
-    const filteredLogsByTokenAddress = logs.filter((log: any) =>
-      isAddressEqual(
-        log.address as `0x${string}`,
-        tempTokenContract.address as `0x${string}`
-      )
-    );
-    const sortedLogs = filteredLogsByTokenAddress.sort(
-      (a, b) => Number(a.blockNumber) - Number(b.blockNumber)
-    );
-    if (sortedLogs.length === 0) return;
-    const latestLog: any = sortedLogs[sortedLogs.length - 1];
-    const newThreshold = latestLog?.args.totalSupplyThreshold as bigint;
-    onThresholdUpdateCallback(newThreshold);
-  };
+  // const handleTempTokenTotalSupplyThresholdUpdatedUpdate = async (
+  //   logs: Log[]
+  // ) => {
+  //   if (logs.length === 0) return;
+  //   const filteredLogsByTokenAddress = logs.filter((log: any) =>
+  //     isAddressEqual(
+  //       log.address as `0x${string}`,
+  //       tempTokenContract.address as `0x${string}`
+  //     )
+  //   );
+  //   const sortedLogs = filteredLogsByTokenAddress.sort(
+  //     (a, b) => Number(a.blockNumber) - Number(b.blockNumber)
+  //   );
+  //   if (sortedLogs.length === 0) return;
+  //   const latestLog: any = sortedLogs[sortedLogs.length - 1];
+  //   const newThreshold = latestLog?.args.totalSupplyThreshold as bigint;
+  //   onThresholdUpdateCallback(newThreshold);
+  // };
 };
