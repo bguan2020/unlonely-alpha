fb01491a04cbe99a1431b3421ac8f19d92148673
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2024-07-07 07:31:42 +0100

    updated frame

--- unlonely-alpha/web/src/pages/channels/[slug].tsx
+++ unlonely-alpha/web/src/pages/channels/[slug].tsx
@@ -1,6 +1,6 @@
 import { GetServerSidePropsContext } from "next";
 import React, { useMemo } from "react";
-import { getFrameMetadata } from "frog/next"
+import { getFrameMetadata, isFrameRequest } from "frog/next"
 import type { Metadata } from "next"
 import { ApolloError } from "@apollo/client";
 
@@ -13,6 +13,8 @@ import { MobilePage } from "../../components/channels/layout/MobilePage";
 import { TempTokenProvider } from "../../hooks/context/useTempToken";
 import { DesktopChannelPageTempToken } from "../../components/channels/layout/temptoken/DesktopChannelPageTempToken";
 import { VersusTempTokenProvider } from "../../hooks/context/useVersusTempToken";
+
+import { headers } from "next/headers";
 
 const ChannelDetail = ({
   channelData,
@@ -29,8 +31,9 @@ const ChannelDetail = ({
     () => channelData?.getChannelBySlug,
     [channelData]
   );
-
+  if (isFrameRequest(headers())) return null
   return (
+    
     <ChannelProvider>
       {!isStandalone ? (
         <TempTokenProvider>
@@ -58,15 +61,22 @@ const ChannelDetail = ({
 };
 
 export default ChannelDetail;
-export async function generateMetadata(): Promise<Metadata> {
-  // const { slug } = context.params!;
-  const channelName = 'slug'
+export async function generateMetadata(context: GetServerSidePropsContext<{ slug: string }>): Promise<Metadata> {
+  const { slug } = context.params!;
   
-  const frameTags = await getFrameMetadata(
-    `${process.env.VERCEL_URL || "http://localhost:3000"}/api/frame/channel/${channelName}`,
-  )
-  return {
-    other: frameTags,
+  const url = `${process.env.NEXT_PUBLIC_HOST_URL || "http://localhost:3000"}/frame/channel/${slug}`;
+  console.log("XXXXXXXXXXXXFetching frame metadata from URL:XXXXXXXXxxxXXXX: ", url);
+
+  try {
+    const frameTags = await getFrameMetadata(url);
+    return {
+      other: frameTags,
+    };
+  } catch (error) {
+    console.error("Error fetching frame metadata:", error);
+    return {
+      other: {},
+    };
   }
 }
 
