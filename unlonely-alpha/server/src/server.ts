30550b4b0c375ce3974d3d7b814485bc0f18ec14
Danny Fung <dannyfung97@gmail.com>
2024-07-29 12:13:24 -0700

    added server timeout in backend

--- unlonely-alpha/server/src/server.ts
+++ unlonely-alpha/server/src/server.ts
@@ -89,11 +89,12 @@ const startServer = async () => {
   // Create an HTTP server using the Express app
   const httpServer = http.createServer(app);
 
-  // force redeploy
+  httpServer.setTimeout(5 * 60 * 1000); // 5 minutes
 
   httpServer.listen(process.env.PORT || 4000, () => {
     console.info(`Server started on port ${process.env.PORT || 4000}`);
   });
+
 };
 
 startServer();
