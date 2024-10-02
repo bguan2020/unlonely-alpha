2fc109231aa595b4dcbee755851fe845c63245d1
Danny Fung <dannyfung97@gmail.com>
2024-10-02 15:24:44 -0700

    Revert "removed package set 1, see if error continues"
    This reverts commit f132711dbe8864561fde1b43b4caf8f511c97770.

---
+++ unlonely-alpha/web/src/hooks/contracts/useZoraCollect1155.ts
@@ -0,0 +1,131 @@
+import { useChainId, usePublicClient, useWalletClient } from "wagmi";
+import { createCollectorClient } from "@zoralabs/protocol-sdk";
+import { Address, encodeFunctionData, TransactionReceipt } from "viem";
+import { zoraCreator1155Abi } from "../../constants/abi/ZoraCreator1155";
+import { SplitV1Client } from "@0xsplits/splits-sdk";
+import {
+  protocolRewardsABI,
+  protocolRewardsAddress,
+} from "@zoralabs/protocol-deployments";
+// import { Aggregate3ValueCall, multicall3Address } from "../../pages/clip";
+import { multicall3Abi } from "../../constants/abi/multicall3";
+import { useCallback } from "react";
+
+export const useZoraCollect1155 = () => {
+  const chainId = useChainId();
+  const publicClient = usePublicClient();
+  const { data: walletClient } = useWalletClient();
+
+  const collectorMint = useCallback(
+    async (
+      tokenContract: `0x${string}`,
+      tokenId: number,
+      quantityToMint: bigint
+    ): Promise<TransactionReceipt | undefined> => {
+      if (!publicClient || !walletClient?.account.address) {
+        console.log("publicClient or walletClient is missing");
+        return undefined;
+      }
+
+      const agregate3Calls: any[] = [];
+
+      const collectorClient = createCollectorClient({ chainId, publicClient });
+
+      const splitAddress = await (publicClient as any).readContract({
+        address: tokenContract,
+        abi: zoraCreator1155Abi,
+        functionName: "getCreatorRewardRecipient",
+        args: [tokenId],
+      });
+
+      console.log("splitAddress", splitAddress);
+
+      const { parameters } = await collectorClient.mint({
+        tokenContract,
+        quantityToMint,
+        mintType: "1155",
+        minterAccount: walletClient.account.address,
+        tokenId,
+      });
+
+      const splitsClient = new SplitV1Client({
+        chainId,
+        publicClient,
+        walletClient,
+        apiConfig: {
+          apiKey: String(process.env.NEXT_PUBLIC_SPLITS_API_KEY),
+        },
+      });
+
+      agregate3Calls.push({
+        allowFailure: false,
+        value: parameters.value || BigInt(0),
+        target: parameters.address,
+        callData: encodeFunctionData({
+          abi: parameters.abi,
+          functionName: parameters.functionName,
+          args: parameters.args,
+        }),
+      });
+
+      const withdrawProtocolRewardsToSplitsCall = {
+        data: encodeFunctionData({
+          abi: protocolRewardsABI,
+          functionName: "withdrawFor",
+          args: [splitAddress, BigInt(0)],
+        }),
+        address:
+          protocolRewardsAddress[
+            chainId as keyof typeof protocolRewardsAddress
+          ],
+      };
+
+      agregate3Calls.push({
+        allowFailure: false,
+        value: BigInt(0),
+        target: withdrawProtocolRewardsToSplitsCall.address,
+        callData: withdrawProtocolRewardsToSplitsCall.data,
+      });
+
+      const withdrawSplitsCall =
+        await splitsClient.callData.batchDistributeAndWithdrawForAll({
+          splitAddress,
+          tokens: ["0x0000000000000000000000000000000000000000"],
+        });
+
+      agregate3Calls.push({
+        allowFailure: false,
+        value: BigInt(0),
+        target: withdrawSplitsCall.address as Address,
+        callData: withdrawSplitsCall.data,
+      });
+
+      // simulate the transaction multicall 3 transaction
+      const { request } = await publicClient.simulateContract({
+        abi: multicall3Abi,
+        functionName: "aggregate3Value",
+        address: "0x1Ee38d535d541c55C9DA5C4f2e3fDf9aDfCf9f9E",
+        args: [agregate3Calls],
+        account: walletClient?.account.address as Address,
+        value: parameters.value || BigInt(0),
+      });
+
+      // execute the transaction
+      const hash = await walletClient
+        ?.writeContract(request)
+        .then((response) => {
+          console.log("multicall3 response", response);
+          return response;
+        });
+      if (!hash) return undefined;
+      const transaction = await publicClient.waitForTransactionReceipt({
+        hash,
+      });
+      console.log("multicall transaction", transaction);
+      return transaction;
+    },
+    [chainId, publicClient, walletClient]
+  );
+
+  return { collectorMint };
+};
---
+++ unlonely-alpha/web/src/hooks/internal/temp-token/write/useSendRemainingFundsToWinnerState.tsx
@@ -0,0 +1,195 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { useNetworkContext } from "../../../context/useNetwork";
+import { ContractData } from "../../../../constants/types";
+import { Box, useToast } from "@chakra-ui/react";
+import Link from "next/link";
+import { useSendRemainingFundsToWinnerAfterTokenExpiration } from "../../../contracts/useTempTokenV1";
+import { isAddress } from "viem";
+import { useChannelContext } from "../../../context/useChannel";
+import {
+  CHAKRA_UI_TX_TOAST_DURATION,
+  InteractionType,
+} from "../../../../constants";
+import { useUser } from "../../../context/useUser";
+import { useApolloClient } from "@apollo/client";
+import { GET_USER_QUERY } from "../../../../constants/queries";
+import centerEllipses from "../../../../utils/centerEllipses";
+import { usePublicClient } from "wagmi";
+import { init, useQuery } from "@airstack/airstack-react";
+import useDebounce from "../../useDebounce";
+import { returnDecodedTopics } from "../../../../utils/contract";
+
+init(String(process.env.NEXT_PUBLIC_AIRSTACK_API_KEY));
+
+const getAddressFromEns = (ens: string) => {
+  return `query GetAddressFromEns {
+    Domains(input: {filter: {name: {_in: ["${ens}"]}}, blockchain: ethereum}) {
+      Domain {
+        resolvedAddress
+        name
+      }
+    }
+  }`;
+};
+
+export const useSendRemainingFundsToWinnerState = (
+  tokenContractData: ContractData,
+  callbackOnTxSuccess?: any
+) => {
+  const { user } = useUser();
+  const toast = useToast();
+  const client = useApolloClient();
+  const publicClient = usePublicClient();
+
+  const { chat } = useChannelContext();
+  const { addToChatbot: addToChatbotForTempToken } = chat;
+  const { network } = useNetworkContext();
+  const { explorerUrl } = network;
+
+  const [winner, setWinner] = useState("");
+  const debouncedWinner = useDebounce(winner, 300);
+  const [resultingWinnerAddress, setResultingWinnerAddress] = useState("");
+
+  const query = useMemo(() => {
+    if (!debouncedWinner || isAddress(debouncedWinner)) return "";
+    return getAddressFromEns(debouncedWinner);
+  }, [debouncedWinner]);
+
+  const { data, loading } = useQuery(query);
+
+  useEffect(() => {
+    if (data?.Domains?.Domain?.[0]?.resolvedAddress) {
+      setResultingWinnerAddress(data.Domains.Domain[0].resolvedAddress);
+    } else {
+      setResultingWinnerAddress(debouncedWinner);
+    }
+  }, [data, debouncedWinner]);
+
+  const {
+    sendRemainingFundsToWinnerAfterTokenExpiration,
+    sendRemainingFundsToWinnerAfterTokenExpirationTxLoading,
+  } = useSendRemainingFundsToWinnerAfterTokenExpiration(
+    {
+      winnerWalletAddress: resultingWinnerAddress,
+    },
+    tokenContractData,
+    {
+      onWriteSuccess: (data) => {
+        toast({
+          render: () => (
+            <Box as="button" borderRadius="md" bg="#287ab0" px={4} h={8}>
+              <Link
+                target="_blank"
+                href={`${explorerUrl}/tx/${data.hash}`}
+                passHref
+              >
+                send remaining funds pending, click to view
+              </Link>
+            </Box>
+          ),
+          duration: CHAKRA_UI_TX_TOAST_DURATION, // chakra ui toast duration
+          isClosable: true,
+          position: "bottom", // chakra ui toast position
+        });
+      },
+      onWriteError: (error) => {
+        toast({
+          duration: CHAKRA_UI_TX_TOAST_DURATION, // chakra ui toast duration
+          isClosable: true,
+          position: "bottom", // chakra ui toast position
+          render: () => (
+            <Box as="button" borderRadius="md" bg="#bd711b" px={4} h={8}>
+              send remaining funds cancelled
+            </Box>
+          ),
+        });
+      },
+      onTxSuccess: async (data) => {
+        const topics = returnDecodedTopics(
+          data.logs,
+          tokenContractData.abi,
+          "SendRemainingFundsToWinnerAfterTokenExpiration"
+        );
+        if (!topics) return;
+        console.log("send remaining funds success", data, topics.args);
+        const args: any = topics.args;
+        const balance = args.balance as bigint;
+        const winnerWallet = args.winnerWallet as `0x${string}`;
+        toast({
+          render: () => (
+            <Box as="button" borderRadius="md" bg="#50C878" px={4} h={8}>
+              <Link
+                target="_blank"
+                href={`${explorerUrl}/tx/${data.transactionHash}`}
+                passHref
+              >
+                send remaining funds success, click to view
+              </Link>
+            </Box>
+          ),
+          duration: CHAKRA_UI_TX_TOAST_DURATION, // chakra ui toast duration
+          isClosable: true,
+          position: "bottom", // chakra ui toast position
+        });
+        const identifiedUser: string = await client
+          .query({
+            query: GET_USER_QUERY,
+            variables: { data: { address: resultingWinnerAddress } },
+          })
+          .then(
+            ({ data }) =>
+              data?.getUser?.username ??
+              centerEllipses(resultingWinnerAddress, 15)
+          );
+        const symbol = await publicClient?.readContract({
+          address: tokenContractData.address as `0x${string}`,
+          abi: tokenContractData.abi,
+          functionName: "symbol",
+          args: [],
+        });
+        const title = `The $${String(
+          symbol
+        )} token balance was sent to ${identifiedUser}!`;
+        addToChatbotForTempToken({
+          username: user?.username ?? "",
+          address: user?.address ?? "",
+          taskType:
+            InteractionType.SEND_REMAINING_FUNDS_TO_WINNER_AFTER_TEMP_TOKEN_EXPIRATION,
+          title,
+          description: JSON.stringify({
+            userAddress: user?.address ?? "",
+            winnerWallet,
+            tokenContractAddress: tokenContractData.address as `0x${string}`,
+            balance: String(balance),
+          }),
+        });
+        setWinner("");
+        callbackOnTxSuccess?.();
+      },
+      onTxError: (error) => {
+        toast({
+          render: () => (
+            <Box as="button" borderRadius="md" bg="#b82929" px={4} h={8}>
+              send remaining funds error
+            </Box>
+          ),
+          duration: CHAKRA_UI_TX_TOAST_DURATION, // chakra ui toast duration
+          isClosable: true,
+          position: "bottom", // chakra ui toast position
+        });
+      },
+    }
+  );
+
+  const handleWinnerChange = useCallback((input: string) => {
+    setWinner(input);
+  }, []);
+
+  return {
+    sendRemainingFundsToWinnerAfterTokenExpiration,
+    loading: loading || sendRemainingFundsToWinnerAfterTokenExpirationTxLoading,
+    handleWinnerChange,
+    winner,
+    resolvedAddress: data?.Domains?.Domain?.[0]?.resolvedAddress ?? undefined,
+  };
+};
