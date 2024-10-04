cabf050b737888772c69fc5bffaeb3234c8d1578
Danny Fung <dannyfung97@gmail.com>
2024-10-04 10:16:27 -0700

    changed tsconfig to bundler

--- unlonely-alpha/web/next.config.js
+++ unlonely-alpha/web/next.config.js
@@ -6,6 +6,7 @@ module.exports = withBundleAnalyzer({
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
+  // transpilePackages: ["@privy-io/react-auth"],
   async redirects() {
     return [
       {
