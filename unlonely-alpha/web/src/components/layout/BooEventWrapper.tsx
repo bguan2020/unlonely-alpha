808c8d90851ab52186c7d65b4e29c357b77db9b5
Danny Fung <dannyfung97@gmail.com>
2024-10-08 10:49:32 -0700

    changed back from countdown

--- unlonely-alpha/web/src/components/layout/BooEventWrapper.tsx
+++ unlonely-alpha/web/src/components/layout/BooEventWrapper.tsx
@@ -37,7 +37,7 @@ const BooEventWrapperWithSolana = () => {
 
   return (
     <>
-      {false ? (
+      {true ? (
         <HomePageBooEventStreamPage />
       ) : (
         <HomePageBooEventTokenCountdown />
