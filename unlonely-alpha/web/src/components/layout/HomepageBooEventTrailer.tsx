f6805d9d34997f65ff419d359cbf05acfca3c6b9
Danny Fung <dannyfung97@gmail.com>
2024-10-14 17:08:38 -0700

    new video link

--- unlonely-alpha/web/src/components/layout/HomepageBooEventTrailer.tsx
+++ unlonely-alpha/web/src/components/layout/HomepageBooEventTrailer.tsx
@@ -31,7 +31,7 @@ export const HomepageBooEventTrailer = ({
     <Flex width={"100%"} height="auto">
       <video
         ref={videoRef}
-        src="https://vod-cdn.lp-playback.studio/raw/jxf4iblf6wlsyor6526t4tcmtmqa/catalyst-vod-com/hls/1186m62d66tdy6i6/720p0.mp4"
+        src="https://vod-cdn.lp-playback.studio/raw/jxf4iblf6wlsyor6526t4tcmtmqa/catalyst-vod-com/hls/8f257gta7zmj1wlg/video/download.mp4"
         autoPlay
         controls
         muted
